a7bd0d4da4a5b1ea64046da817f3d4fa0b632dba
Sergey Odinokov <odinserj@gmail.com>
2013-08-07 19:37:40 +0400

    Small processor improvings
    1. Call Worker.Dispose
    2. _currentJob is now volatile
    3. Removing obsolete TODOs

--- HangFire/Processor.cs
+++ HangFire/Processor.cs
@@ -6,7 +6,6 @@ namespace HangFire
 {
     internal class Processor
     {
-        // TODO: change name corresponding to the processor name
         private readonly ILog _logger = LogManager.GetLogger(typeof (Processor));
 
         private readonly Manager _manager;
@@ -14,8 +13,7 @@ namespace HangFire
         private readonly ManualResetEventSlim _jobIsReady 
             = new ManualResetEventSlim(false);
 
-        // TODO: does ManualResetEventSlim provides memory barrier?
-        private string _currentJob;
+        private volatile string _currentJob;
 
         public Processor(Manager manager)
         {
@@ -28,17 +26,9 @@ namespace HangFire
             _thread.Start();
         }
 
-        public void Stop()
-        {
-            
-        }
-
-        public void Wait()
-        {
-        }
-
         public void Process(string serializedJob)
         {
+            // TODO: Possible race condition
             _currentJob = serializedJob;
             _jobIsReady.Set();
         }
@@ -48,31 +38,22 @@ namespace HangFire
             while (true)
             {
                 _manager.NotifyFreeProcessor(this);
-                // TODO: handle manager stop.
                 _jobIsReady.Wait();
 
                 try
                 {
-                    // TODO: add deserialization exception handling. It does no sense to restart them.
                     var job = JsonHelper.Deserialize<Job>(_currentJob);
 
-                    // TODO: handle activation errors. It does no sense to restart them.
-                    var worker = Factory.CreateWorker(job.WorkerType);
-                    worker.Args = job.Args;
+                    using (var worker = Factory.CreateWorker(job.WorkerType))
+                    {
+                        worker.Args = job.Args;
 
-                    // TODO: add user code exception handling. And restart it if possible.
-                    worker.Perform();
+                        // TODO: server middleware
+                        worker.Perform();
+                    }
                 }
                 catch (Exception ex)
                 {
-                    // Возможные причины:
-                    // 1. Произошла ошибка при десериализации.
-                    //    - Записан какой-то мусор.
-                    //    - Старый формат класса Job.
-                    // 2. Ошибка при создании экземпляра нужного Worker'а.
-                    //    - Невозможно найти нужный класс
-                    //    - Невозможно сконструировать нужный класс
-                    // 3. Произошла необработанная ошибка в пользовательском коде Worker'а.
                     _logger.Error(
                         "Failed to process the job: unexpected exception caught. Job JSON:"
                         + Environment.NewLine
