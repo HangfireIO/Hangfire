4b89b0d66b9133d9354c26a95ad834fc6cd299c4
Sergey Odinokov <odinserj@gmail.com>
2013-08-07 19:38:31 +0400

    Sample ASP.NET MVC Application

---
+++ HangFire.Sample/App_Start/HangFireConfig.cs
@@ -0,0 +1,13 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(
+    typeof(HangFire.Sample.App_Start.HangFireConfig), "PreStart")]
+
+namespace HangFire.Sample.App_Start
+{
+    public static class HangFireConfig
+    {
+        public static void PreStart()
+        {
+            
+        }
+    }
+}
---
+++ HangFire.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HangFire.Sample.MvcApplication" Language="C#" %>
