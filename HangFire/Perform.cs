88055b6195c5f7927be48f385a23612ca7939707
odinserj <odinserj@chessok.com>
2013-08-15 15:51:50 +0400

    Removed Client.cs

--- HangFire/Perform.cs
+++ HangFire/Perform.cs
@@ -1,7 +1,13 @@
-﻿namespace HangFire
+﻿using System;
+
+using BookSleeve;
+
+namespace HangFire
 {
     public static class Perform
     {
+        private static readonly RedisConnection _redis = RedisClient.CreateConnection();
+
         public static void Async<TWorker>()
             where TWorker : Worker
         {
@@ -11,7 +17,17 @@
         public static void Async<TWorker>(object arg)
             where TWorker : Worker
         {
-            Client.Enqueue(typeof (TWorker), arg);
+            var job = new Job(typeof(TWorker), arg);
+
+            // TODO: handle serialization exceptions.
+            // Either properties or args can not be serialized if
+            // it's type is unserializable. We need to throw this
+            // exception to the client.
+            var serialized = JsonHelper.Serialize(job);
+
+            // TODO: handle Redis exception. Do we need to try it again?
+            var result = _redis.Lists.AddFirst(0, String.Format("queue:default"), serialized);
+            _redis.Wait(result);
         }
     }
 }
