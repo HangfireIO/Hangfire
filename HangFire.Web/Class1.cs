68dfcef47b0745e7428857b4943dad8a46b874a3
Sergey Odinokov <odinserj@gmail.com>
2013-08-17 18:15:54 +0400

    Added HangFire.Web & HangFire.MvcSample

---
+++ HangFire.Web/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangFire.Web
+{
+    public class Class1
+    {
+    }
+}
