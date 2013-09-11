2aca66f89ef11d045196573d54ae6bae1ce0b140
Sergey Odinokov <odinserj@gmail.com>
2013-09-11 19:12:43 +0400

    Integration tests refactoring

--- HangFire.Tests/IntegrationTests.cs
+++ HangFire.Tests/IntegrationTests.cs
@@ -1,66 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Moq;
-
-using ServiceStack.Logging;
-using ServiceStack.Logging.Support.Logging;
-using ServiceStack.Redis;
-
 namespace HangFire.Tests
 {
     [TestClass]
-    public class IntegrationTests
+    public class IntegrationTests : RedisPoweredTest
     {
-        private const int RedisDb = 5;
-        private const string RedisHost = "localhost";
-        private const int RedisPort = 6379;
-
         private HangFireServer _server;
-        private IRedisClient _redis;
-
-        private static readonly object _lock = new object();
         private static bool _performed;
 
-        [TestInitialize]
-        public void SetUp()
+        protected override void Initialize()
         {
-            Monitor.Enter(_lock);
-
-            _redis = new ServiceStack.Redis.RedisClient(RedisHost, RedisPort, null, RedisDb);
-            _redis.FlushDb();
-
-            LogManager.LogFactory = new ConsoleLogFactory();
-
-            HangFireConfiguration.Configure(
-                x =>
-                {
-                    x.RedisDb = RedisDb;
-                    x.RedisHost = RedisHost;
-                    x.RedisPort = RedisPort;
-                });
-
             _server = new HangFireServer("TestServer", "default", 1, TimeSpan.FromMilliseconds(500));
-
             _performed = false;
         }
 
-        [TestCleanup]
-        public void TearDown()
+        protected override void CleanUp()
         {
-            try
-            {
-                _server.Dispose();
-                _redis.Dispose();
-            }
-            finally
-            {
-                Monitor.Exit(_lock);
-            }   
+            _server.Dispose();
         }
 
         [TestMethod]
---
+++ HangFire.Tests/RedisPoweredTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ServiceStack.Logging;
+using ServiceStack.Logging.Support.Logging;
+using ServiceStack.Redis;
+
+namespace HangFire.Tests
+{
+    [TestClass]
+    public abstract class RedisPoweredTest
+    {
+        protected const int RedisDb = 5;
+        protected const string RedisHost = "localhost";
+        protected const int RedisPort = 6379;
+
+        protected IRedisClient Redis;
+
+        private static readonly object _lock = new object();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Monitor.Enter(_lock);
+            LogManager.LogFactory = new ConsoleLogFactory();
+
+            Redis = new ServiceStack.Redis.RedisClient(RedisHost, RedisPort, null, RedisDb);
+            Redis.FlushDb();
+
+            HangFireConfiguration.Configure(
+                x =>
+                {
+                    x.RedisDb = RedisDb;
+                    x.RedisHost = RedisHost;
+                    x.RedisPort = RedisPort;
+                });
+
+            Initialize();
+        }
+
+        [TestCleanup]
+        public virtual void TearDown()
+        {
+            try
+            {
+                CleanUp();
+                Redis.Dispose();
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
+        }
+
+        protected virtual void Initialize()
+        {
+        }
+
+        protected virtual void CleanUp()
+        {
+        }
+    }
+}
