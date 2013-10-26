97cf59958fbad37829ad38aabbe59f243bcb3372
Sergey Odinokov <odinserj@gmail.com>
2013-10-26 19:53:43 +0400

    Docs for the AspNetBackgroundJobServer class

--- HangFire.Web/AspNetBackgroundJobServer.cs
+++ HangFire.Web/AspNetBackgroundJobServer.cs
@@ -3,16 +3,29 @@
 namespace HangFire.Web
 {
     /// <summary>
-    /// Represents the HangFire server that designed specifically to
-    /// work within the ASP.NET application.
+    /// Represents the HangFire server that implements the
+    /// <see cref="IRegisteredObject"/> interface. 
     /// </summary>
     public class AspNetBackgroundJobServer : BackgroundJobServer, IRegisteredObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AspNetBackgroundJobServer"/>
+        /// class with the number of workers and the list of queues that will 
+        /// be processed by this instance of a server.
+        /// </summary>
+        /// <param name="workerCount">The number of workers.</param>
+        /// <param name="queues">The list of queues that will be processed.</param>
         public AspNetBackgroundJobServer(int workerCount, params string[] queues)
             : base(workerCount, queues)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AspNetBackgroundJobServer"/>
+        /// class with the default number of workers and the specified list of
+        /// queues that will be processed by this instance of a server.
+        /// </summary>
+        /// <param name="queues">The list of queues that will be processed.</param>
         public AspNetBackgroundJobServer(params string[] queues)
             : base(queues)
         {
