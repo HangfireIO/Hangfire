ab66d49afece938ae724f190baa7b1458d6c086f
Sergey Odinokov <odinserj@gmail.com>
2013-11-10 13:06:07 +0400

    Updated *.nuspec

--- HangFire.Web/Properties/AssemblyInfo.cs
+++ HangFire.Web/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HangFire.Web")]
-[assembly: AssemblyDescription("Background jobs processing for ASP.NET apps")]
+[assembly: AssemblyDescription("Background job processing for ASP.NET applications.")]
 [assembly: Guid("f2e3853b-743a-439b-bc2b-66fac411cb43")]
--- HangFire/Properties/AssemblyInfo.cs
+++ HangFire/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HangFire")]
-[assembly: AssemblyDescription("Background job processing for ASP.NET apps")]
+[assembly: AssemblyDescription("Core classes of HangFire that are independent of any framework.")]
 [assembly: Guid("4deecd4f-19f6-426b-aa87-6cd1a03eaa48")]
 [assembly: InternalsVisibleTo("HangFire.Tests")]
 
