776f51257bda7fcdce1e9d48a8bf52c2f2523ef6
Sergey Odinokov <odinserj@gmail.com>
2013-11-10 13:53:19 +0400

    License notice for each file

--- HangFire/Filters/Client/CreatingContext.cs
+++ HangFire/Filters/Client/CreatingContext.cs
@@ -1,4 +1,20 @@
-﻿using HangFire.Client;
+﻿// This file is part of HangFire.
+// Copyright © 2013 Sergey Odinokov.
+// 
+// HangFire is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// HangFire is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with HangFire.  If not, see <http://www.gnu.org/licenses/>.
+
+using HangFire.Client;
 
 namespace HangFire.Filters
 {
