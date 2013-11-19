d8df632a05e4ec6ce43bff8bb7231dd08aaa8428
Sergey Odinokov <odinserj@gmail.com>
2013-11-19 17:29:21 +0400

    Bump version to 0.5.2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyInformationalVersion("0.5.0")]
+[assembly: AssemblyVersion("0.5.2")]
+[assembly: AssemblyInformationalVersion("0.5.2")]
