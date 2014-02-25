18e8233aef342682170f15a86a16021f8dfba96b
Sergey Odinokov <odinserj@gmail.com>
2014-02-25 21:58:17 +0400

    Changed copyright year

--- HangFire.Redis/PrefetchJobFetcher.cs
+++ HangFire.Redis/PrefetchJobFetcher.cs
@@ -1,5 +1,5 @@
 ﻿// This file is part of HangFire.
-// Copyright © 2013 Sergey Odinokov.
+// Copyright © 2013-2014 Sergey Odinokov.
 // 
 // HangFire is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("HangFire")]
 [assembly: AssemblyCompany("Sergey Odinokov")]
-[assembly: AssemblyCopyright("Copyright © 2013, Sergey Odinokov")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 Sergey Odinokov")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
