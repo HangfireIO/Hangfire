00cef2eab77bd5ae44d9a2da022d39c61b7c364e
Sergey Odinokov <odinserj@gmail.com>
2014-03-09 23:47:17 +0400

    Short description for HangFire.SqlServer assembly

--- HangFire.SqlServer/Properties/AssemblyInfo.cs
+++ HangFire.SqlServer/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HangFire.SqlServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SQL Server job storage for HangFire")]
 [assembly: Guid("3d96bf2f-8854-4872-aee3-faf81d121a4d")]
 
 // Allow the generation of mocks for internal types
