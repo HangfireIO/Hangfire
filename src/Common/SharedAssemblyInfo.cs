7557af31cb5c7035116759fa48f8ebb30e3f5d76
Sergey Odinokov <odinserj@gmail.com>
2014-03-13 19:59:53 +0400

    Bump version to 0.7.0-alpha1

--- src/Common/SharedAssemblyInfo.cs
+++ src/Common/SharedAssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("HangFire")]
 [assembly: AssemblyCompany("Sergey Odinokov")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014 Sergey Odinokov")]
+[assembly: AssemblyCopyright("Copyright Â© 2013-2014 Sergey Odinokov")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("0.6.2")]
-[assembly: AssemblyInformationalVersion("0.6.2")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0-alpha1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
