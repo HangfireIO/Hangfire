d90bba48d21013a4112a4abd1b4764129cd24164
Sergey Odinokov <odinserj@gmail.com>
2014-03-15 14:25:06 +0400

    Removing duplicate compilation messages, part 2

--- build.cmd
+++ build.cmd
@@ -7,4 +7,4 @@ set config=%2
 if "%config%" == "" (
    set config=Debug
 )
-msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal;Encoding=UTF-8 /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal;Encoding=UTF-8 /nr:false
--- build/AnalyzeBuildLog.ps1
+++ build/AnalyzeBuildLog.ps1
@@ -3,6 +3,10 @@ param($logFile)
 "Start analyzing the build log..."
 
 $reader = [System.IO.File]::OpenText($logFile)
+
+Add-Type -AssemblyName System.Core
+$h = new-object 'System.Collections.Generic.HashSet[string]'
+
 try {
     for(;;) {
         $line = $reader.ReadLine()
@@ -12,17 +16,21 @@ try {
             $projectFile = $matches.ProjectName + "." + $matches.ProjectExt
             $category = $matches.Severity.substring(0,1).toupper() + $matches.Severity.substring(1).tolower()
             
-            appveyor AddCompilationMessage `
-              -Message $matches.Message `
-              -Details $matches.Message `
-              -Category $category `
-              -FileName $matches.FileName `
-              -Line $matches.Line `
-              -Column $matches.Column `
-              -ProjectName $matches.ProjectName `
-              -ProjectFile $projectFile
-
-           
+            $key = $matches.ProjectName + '|' + $matches.FileName + '|' + $matches.Line + '|' + $matches.Column + '|' + $matches.Message
+            
+            if (-Not ($h.Contains($key))) {
+              $h.Add($key)
+              
+              appveyor AddCompilationMessage `
+                -Message $matches.Message `
+                -Details $matches.Message `
+                -Category $category `
+                -FileName $matches.FileName `
+                -Line $matches.Line `
+                -Column $matches.Column `
+                -ProjectName $matches.ProjectName `
+                -ProjectFile $projectFile
+            }
         }
     }
 }
