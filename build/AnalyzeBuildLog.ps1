8903525230776c507035190630ae1e8eb1f0c4b9
Sergey Odinokov <odinserj@gmail.com>
2014-03-15 14:32:54 +0400

    Simplifying analyze log commandlet

--- build/AnalyzeBuildLog.ps1
+++ build/AnalyzeBuildLog.ps1
@@ -13,23 +13,21 @@ try {
         if ($line -eq $null) { break }
         
         if ($line -match '^[\s]*(?<FileName>(?![\s]*[\d]+\>).+)\((?<Line>[\d]+),(?<Column>[\d]+)\): (?<Severity>warning|error|information) (?<Code>[A-Z0-9]+): (?<Message>.*) \[(?<ProjectDir>.+)\\(?<ProjectName>.+)\.(?<ProjectExt>.+)\]$') {
-            $projectFile = $matches.ProjectName + "." + $matches.ProjectExt
-            $category = $matches.Severity.substring(0,1).toupper() + $matches.Severity.substring(1).tolower()
+            $keys = $matches.ProjectName, $matches.FileName, $matches.Line, $matches.Column, $matches.Message
+
+            if ($h.Add($keys -join '|')) {
+                $projectFile = $matches.ProjectName + "." + $matches.ProjectExt
+                $category = $matches.Severity.substring(0,1).toupper() + $matches.Severity.substring(1).tolower()
             
-            $key = $matches.ProjectName + '|' + $matches.FileName + '|' + $matches.Line + '|' + $matches.Column + '|' + $matches.Message
-            
-            if (-Not ($h.Contains($key))) {
-              $h.Add($key)
-              
-              appveyor AddCompilationMessage `
-                -Message $matches.Message `
-                -Details $matches.Message `
-                -Category $category `
-                -FileName $matches.FileName `
-                -Line $matches.Line `
-                -Column $matches.Column `
-                -ProjectName $matches.ProjectName `
-                -ProjectFile $projectFile
+                appveyor AddCompilationMessage `
+                    -Message $matches.Message `
+                    -Details $matches.Message `
+                    -Category $category `
+                    -FileName $matches.FileName `
+                    -Line $matches.Line `
+                    -Column $matches.Column `
+                    -ProjectName $matches.ProjectName `
+                    -ProjectFile $projectFile
             }
         }
     }
