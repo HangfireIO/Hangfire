39ad8fa93246f18649f8860ac76bbcddba8cd725
Sergey Odinokov <odinserj@gmail.com>
2014-03-22 23:25:52 +0400

    Added LGPLv3 headers to HangFire.SqlServer

--- src/HangFire.SqlServer/Install.sql
+++ src/HangFire.SqlServer/Install.sql
@@ -1,4 +1,20 @@
-﻿DECLARE @TARGET_SCHEMA_VERSION INT;
+﻿-- This file is part of HangFire.
+-- Copyright © 2013-2014 Sergey Odinokov.
+-- 
+-- HangFire is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as 
+-- published by the Free Software Foundation, either version 3 
+-- of the License, or any later version.
+-- 
+-- HangFire is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public 
+-- License along with HangFire. If not, see <http://www.gnu.org/licenses/>.
+
+DECLARE @TARGET_SCHEMA_VERSION INT;
 SET @TARGET_SCHEMA_VERSION = 1;
 
 PRINT 'Installing HangFire SQL objects...';
