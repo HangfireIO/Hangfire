97cfeb8651cd08773618a31746aff012c3fe0aff
Sergey Odinokov <odinserj@gmail.com>
2014-03-22 23:20:41 +0400

    Fixed LGPLv3 file headers

--- src/HangFire.Core/Server/Filters/PerformingContext.cs
+++ src/HangFire.Core/Server/Filters/PerformingContext.cs
@@ -2,17 +2,17 @@
 // Copyright © 2013-2014 Sergey Odinokov.
 // 
 // HangFire is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// it under the terms of the GNU Lesser General Public License as 
+// published by the Free Software Foundation, either version 3 
+// of the License, or any later version.
 // 
 // HangFire is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 // 
-// You should have received a copy of the GNU General Public License
-// along with HangFire.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public 
+// License along with HangFire. If not, see <http://www.gnu.org/licenses/>.
 
 using HangFire.Server.Performing;
 
