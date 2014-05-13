83d6eee0abe28cb7e0a56d8a5c052536b64a8fdc
Sergey Odinokov <odinserj@gmail.com>
2014-05-13 18:26:15 +0400

    Added integration tests for MSMQ

--- tests/HangFire.SqlServer.Tests/MessageQueueJobQueueFacts.cs
+++ tests/HangFire.SqlServer.Tests/MessageQueueJobQueueFacts.cs
@@ -9,6 +9,13 @@ namespace HangFire.SqlServer.Tests
 {
     public class MessageQueueJobQueueFacts
     {
+        private readonly CancellationToken _token;
+
+        public MessageQueueJobQueueFacts()
+        {
+            _token = new CancellationToken();
+        }
+
         [Fact]
         public void Ctor_ThrowsAnException_WhenPathPatternIsNull()
         {
@@ -54,11 +61,86 @@ namespace HangFire.SqlServer.Tests
         {
             EnqueueJobId("my-queue", "job-id");
             var queue = CreateQueue();
-            var token = new CancellationToken();
 
-            var fetchedJob = queue.Dequeue(new[] { "my-queue" }, token);
+            var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
 
             Assert.Equal("job-id", fetchedJob.JobId);
+        }
+
+        [Fact, CleanMsmqQueue("my-queue")]
+        public void Dequeue_ThrowsCanceledException_WhenTokenHasBeenCancelled()
+        {
+            var queue = CreateQueue();
+            var token = new CancellationToken(true);
+
+            Assert.Throws<OperationCanceledException>(
+                () => queue.Dequeue(new[] { "my-queue" }, token));
+        }
+
+        [Fact, CleanMsmqQueue("queue-1", "queue-2")]
+        public void Dequeue_ReturnsFetchedJob_FromOtherQueues_IfFirstAreEmpty()
+        {
+            EnqueueJobId("queue-2", "job-id");
+            var queue = CreateQueue();
+
+            var fetchedJob = queue.Dequeue(new[] { "queue-1", "queue-2" }, _token);
+
+            Assert.Equal("job-id", fetchedJob.JobId);
+        }
+
+        [Fact, CleanMsmqQueue("my-queue")]
+        public void Dequeue_MakesJobInvisibleForOtherFetchers()
+        {
+            // Arrange
+            EnqueueJobId("my-queue", "job-id");
+            var queue = CreateQueue();
+
+            // Act
+            var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+
+            // Assert
+            Assert.NotNull(fetchedJob);
+
+            var exception = Assert.Throws<MessageQueueException>(
+                () => DequeueJob("my-queue", TimeSpan.FromSeconds(1)));
+
+            Assert.Equal(MessageQueueErrorCode.IOTimeout, exception.MessageQueueErrorCode);
+        }
+
+        [Fact, CleanMsmqQueue("my-queue")]
+        public void RemoveFromQueue_OnFetchedJob_RemovesTheJobCompletely()
+        {
+            // Arrange
+            EnqueueJobId("my-queue", "job-id");
+            var queue = CreateQueue();
+
+            // Act
+            using (var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token))
+            {
+                fetchedJob.RemoveFromQueue();
+            }
+
+            // Assert
+            var exception = Assert.Throws<MessageQueueException>(
+                () => DequeueJob("my-queue", TimeSpan.FromSeconds(5)));
+
+            Assert.Equal(MessageQueueErrorCode.IOTimeout, exception.MessageQueueErrorCode);
+        }
+
+        [Fact, CleanMsmqQueue("my-queue")]
+        public void DisposeWithoutRemoval_OnFetchedJob_ReturnsTheJobToTheQueue()
+        {
+            // Arrange
+            EnqueueJobId("my-queue", "job-id");
+            var queue = CreateQueue();
+
+            // Act
+            var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+            fetchedJob.Dispose();
+
+            // Assert
+            var jobId = DequeueJob("my-queue", TimeSpan.FromSeconds(5));
+            Assert.Equal("job-id", jobId);
         }
 
         private static void EnqueueJobId(string queue, string jobId)
@@ -70,6 +152,23 @@ namespace HangFire.SqlServer.Tests
                 transaction.Begin();
                 messageQueue.Send(message, transaction);
                 transaction.Commit();
+            }
+        }
+
+        private static string DequeueJob(string queue, TimeSpan timeout)
+        {
+            using (var messageQueue = CleanMsmqQueueAttribute.GetMessageQueue(queue))
+            using (var transaction = new MessageQueueTransaction())
+            {
+                transaction.Begin();
+                
+                using (var message = messageQueue.Receive(timeout, transaction))
+                {
+                    message.Formatter = new BinaryMessageFormatter();
+                    transaction.Commit();
+
+                    return (string) message.Body;
+                }
             }
         }
 
