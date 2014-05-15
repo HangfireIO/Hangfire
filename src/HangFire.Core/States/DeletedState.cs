76a1cf06805b418ed61f876cd04263a108c7bb94
Sergey Odinokov <odinserj@gmail.com>
2014-05-15 19:09:08 +0400

    Added the DeletedState class

---
+++ src/HangFire.Core/States/DeletedState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HangFire.Common;
+
+namespace HangFire.States
+{
+    public class DeletedState : IState
+    {
+        public static readonly string StateName = "Deleted";
+
+        public DeletedState()
+        {
+            DeletedAt = DateTime.UtcNow;
+        }
+
+        public string Name
+        {
+            get { return StateName; }
+        }
+
+        public string Reason { get; set; }
+
+        public bool IsFinal
+        {
+            get { return true; }
+        }
+
+        public DateTime DeletedAt { get; private set; }
+
+        public Dictionary<string, string> SerializeData()
+        {
+            return new Dictionary<string, string>
+            {
+                { "DeletedAt", JobHelper.ToStringTimestamp(DeletedAt) }
+            };
+        }
+    }
+}
