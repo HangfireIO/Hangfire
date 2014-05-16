bf010b8cab5d53d4cc156511ccbb36b3ab049a1b
Sergey Odinokov <odinserj@gmail.com>
2014-05-17 00:37:57 +0400

    Bump version to 0.8.1

--- src/Common/SharedAssemblyInfo.cs
+++ src/Common/SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyVersion("0.8.1")]
+[assembly: AssemblyInformationalVersion("0.8.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
