3c30e2d5ab8b1c3846cc5dfc1ea066754d1a4b28
Sergey Odinokov <odinserj@gmail.com>
2014-05-20 20:59:11 +0400

    Add filter that prevents concurrent job execution

---
+++ src/HangFire.Core/DisableConcurrentExecutionAttribute.cs
@@ -0,0 +1,47 @@
+﻿// This file is part of HangFire.
+// Copyright © 2013-2014 Sergey Odinokov.
+// 
+// HangFire is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as 
+// published by the Free Software Foundation, either version 3 
+// of the License, or any later version.
+// 
+// HangFire is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public 
+// License along with HangFire. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using HangFire.Common;
+using HangFire.Server;
+
+namespace HangFire
+{
+    public class DisableConcurrentExecutionAttribute : JobFilterAttribute, IServerFilter
+    {
+        public void OnPerforming(PerformingContext filterContext)
+        {
+            var resource = String.Format(
+                "{0}.{1}",
+                filterContext.Job.Type.FullName,
+                filterContext.Job.Method.Name);
+
+            var distributedLock = filterContext.Connection.AcquireDistributedLock(resource);
+            filterContext.Items["DistributedLock"] = distributedLock;
+        }
+
+        public void OnPerformed(PerformedContext filterContext)
+        {
+            if (!filterContext.Items.ContainsKey("DistributedLock"))
+            {
+                throw new InvalidOperationException("Can not release a distributed lock: it was not acquired.");
+            }
+
+            var distributedLock = (IDisposable)filterContext.Items["DistributedLock"];
+            distributedLock.Dispose();
+        }
+    }
+}
