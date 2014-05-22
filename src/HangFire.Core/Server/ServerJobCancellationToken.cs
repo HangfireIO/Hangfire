a0c9eba3f774339ad641f76fc4556254b5bdff01
Sergey Odinokov <odinserj@gmail.com>
2014-05-22 13:56:16 +0400

    Add types for job cancellation token

---
+++ src/HangFire.Core/Server/ServerJobCancellationToken.cs
@@ -0,0 +1,64 @@
+// This file is part of HangFire.
+// Copyright © 2013-2014 Sergey Odinokov.
+// 
+// HangFire is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as 
+// published by the Free Software Foundation, either version 3 
+// of the License, or any later version.
+// 
+// HangFire is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public 
+// License along with HangFire. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Threading;
+using HangFire.Storage;
+
+namespace HangFire.Server
+{
+    internal class ServerJobCancellationToken : IJobCancellationToken
+    {
+        private readonly string _jobId;
+        private readonly CancellationToken _shutdownToken;
+        private readonly IStorageConnection _connection;
+
+        public ServerJobCancellationToken(
+            string jobId,
+            IStorageConnection connection,
+            CancellationToken shutdownToken)
+        {
+            if (jobId == null) throw new ArgumentNullException("jobId");
+            if (connection == null) throw new ArgumentNullException("connection");
+
+            _jobId = jobId;
+            _shutdownToken = shutdownToken;
+            _connection = connection;
+        }
+
+        public bool IsCancellationRequested
+        {
+            get { return _shutdownToken.IsCancellationRequested || IsJobAborted(); }
+        }
+
+        public void ThrowIfCancellationRequested()
+        {
+            _shutdownToken.ThrowIfCancellationRequested();
+
+            if (IsJobAborted())
+            {
+                throw new JobAbortedException();
+            }
+        }
+
+        private bool IsJobAborted()
+        {
+            var isAborted = _connection.GetJobParameter(_jobId, "AbortRequested");
+
+            return "true".Equals(isAborted, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ tests/HangFire.Core.Tests/Server/ServerJobCancellationTokenFacts.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading;
+using HangFire.Server;
+using HangFire.Storage;
+using Moq;
+using Xunit;
+
+namespace HangFire.Core.Tests.Server
+{
+    public class ServerJobCancellationTokenFacts
+    {
+        private const string JobId = "my-job";
+        private readonly Mock<IStorageConnection> _connection;
+        private CancellationToken _shutdownToken;
+
+        public ServerJobCancellationTokenFacts()
+        {
+            _connection = new Mock<IStorageConnection>();
+            _shutdownToken = new CancellationToken(false);
+        }
+
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenJobIsIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new ServerJobCancellationToken(null, _connection.Object, new CancellationToken()));
+
+            Assert.Equal("jobId", exception.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenConnectionIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new ServerJobCancellationToken(JobId, null, new CancellationToken()));
+
+            Assert.Equal("connection", exception.ParamName);
+        }
+
+        [Fact]
+        public void IsCancellationRequested_ReturnsTrue_WithoutStorageAccess_IfItWasRequestedByShutdown()
+        {
+            _shutdownToken = new CancellationToken(true);
+            var token = CreateToken();
+
+            var result = token.IsCancellationRequested;
+
+            Assert.True(result);
+            _connection.Verify(
+                x => x.GetJobParameter(It.IsAny<string>(), It.IsAny<string>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public void IsCancellationRequested_ReturnsTrue_IfAJobWasAborted()
+        {
+            _connection.Setup(x => x.GetJobParameter(JobId, "AbortRequested")).Returns("true");
+            var token = CreateToken();
+
+            var result = token.IsCancellationRequested;
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsCancellationRequested_ReturnsFalse_IfAbortedParameterIsNull()
+        {
+            _connection.Setup(x => x.GetJobParameter(JobId, "AbortRequested")).Returns((string)null);
+            var token = CreateToken();
+
+            var result = token.IsCancellationRequested;
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsCancellationRequested_ReturnsFalse_IfAbortedParameterIsFalse()
+        {
+            _connection.Setup(x => x.GetJobParameter(JobId, "AbortRequested")).Returns("false");
+            var token = CreateToken();
+
+            var result = token.IsCancellationRequested;
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ThrowIfCancellationRequested_DoesNotThrow_IfNeitherShutdownNorAbortRequested()
+        {
+            var token = CreateToken();
+
+            Assert.DoesNotThrow(token.ThrowIfCancellationRequested);
+        }
+
+        [Fact]
+        public void ThrowIfCancellationRequested_ThrowsOperationCanceled_OnShutdownRequest()
+        {
+            _shutdownToken = new CancellationToken(true);
+            var token = CreateToken();
+
+            Assert.Throws<OperationCanceledException>(
+                () => token.ThrowIfCancellationRequested());
+        }
+
+        [Fact]
+        public void ThrowIfCancellationRequested_ThrowsJobAborted_OnAbort()
+        {
+            _connection.Setup(x => x.GetJobParameter(JobId, "AbortRequested")).Returns("true");
+            var token = CreateToken();
+
+            Assert.Throws<JobAbortedException>(
+                () => token.ThrowIfCancellationRequested());
+        }
+
+        private IJobCancellationToken CreateToken()
+        {
+            return new ServerJobCancellationToken(JobId, _connection.Object, _shutdownToken);
+        }
+    }
+}
