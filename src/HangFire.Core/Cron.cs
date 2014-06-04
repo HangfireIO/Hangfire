ad665264e1a6e0db8390c7d1b2f00f3998832345
Sergey Odinokov <odinserj@gmail.com>
2014-06-04 19:16:11 +0400

    Cron class returns strings instead of CronSchedule

--- src/HangFire.Core/Cron.cs
+++ src/HangFire.Core/Cron.cs
@@ -15,225 +15,212 @@
 // License along with HangFire. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using NCrontab;
 
 namespace HangFire
 {
     /// <summary>
-    /// Helper class that provides common values for the
-    /// <see cref="CrontabSchedule"/> class.
+    /// Helper class that provides common values for the cron expressions.
     /// </summary>
     public class Cron
     {
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every minute.
+        /// Returns cron expression that fires every minute.
         /// </summary>
-        public static CrontabSchedule Minutely()
+        public static string Minutely()
         {
-            return Parse("* * * * *");
+            return "* * * * *";
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every hour at the first minute.
+        /// Returns cron expression that fires every hour at the first minute.
         /// </summary>
-        public static CrontabSchedule Hourly()
+        public static string Hourly()
         {
             return Hourly(minute: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every hour at the specified minute.
+        /// Returns cron expression that fires every hour at the specified minute.
         /// </summary>
         /// <param name="minute">The minute in which the schedule will be activated (0-59).</param>
-        public static CrontabSchedule Hourly(int minute)
+        public static string Hourly(int minute)
         {
-            return Parse(String.Format("{0} * * * *", minute));
+            return String.Format("{0} * * * *", minute);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every day at 00:00 UTC.
+        /// Returns cron expression that fires every day at 00:00 UTC.
         /// </summary>
-        public static CrontabSchedule Daily()
+        public static string Daily()
         {
             return Daily(hour: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every day at the first minute of the specified hour in UTC.
+        /// Returns cron expression that fires every day at the first minute of
+        /// the specified hour in UTC.
         /// </summary>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
-        public static CrontabSchedule Daily(int hour)
+        public static string Daily(int hour)
         {
             return Daily(hour, minute: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every day at the specified hour and minute in UTC.
+        /// Returns cron expression that fires every day at the specified hour and minute
+        /// in UTC.
         /// </summary>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
         /// <param name="minute">The minute in which the schedule will be activated (0-59).</param>
-        public static CrontabSchedule Daily(int hour, int minute)
+        public static string Daily(int hour, int minute)
         {
-            return Parse(String.Format("{0} {1} * * *", minute, hour));
+            return String.Format("{0} {1} * * *", minute, hour);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every week at Monday, 00:00 UTC.
+        /// Returns cron expression that fires every week at Monday, 00:00 UTC.
         /// </summary>
-        public static CrontabSchedule Weekly()
+        public static string Weekly()
         {
             return Weekly(DayOfWeek.Monday);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every week at 00:00 UTC of the specified day of week and hour.
+        /// Returns cron expression that fires every week at 00:00 UTC of the specified
+        /// day of the week.
         /// </summary>
         /// <param name="dayOfWeek">The day of week in which the schedule will be activated.</param>
-        public static CrontabSchedule Weekly(DayOfWeek dayOfWeek)
+        public static string Weekly(DayOfWeek dayOfWeek)
         {
             return Weekly(dayOfWeek, hour: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every week at the first minute of the specified day of 
-        /// week and hour (in UTC).
+        /// Returns cron expression that fires every week at the first minute
+        /// of the specified day of week and hour in UTC.
         /// </summary>
         /// <param name="dayOfWeek">The day of week in which the schedule will be activated.</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
-        public static CrontabSchedule Weekly(DayOfWeek dayOfWeek, int hour)
+        public static string Weekly(DayOfWeek dayOfWeek, int hour)
         {
             return Weekly(dayOfWeek, hour, minute: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every week at the specified day of week, hour and minute (in UTC).
+        /// Returns cron expression that fires every week at the specified day
+        /// of week, hour and minute in UTC.
         /// </summary>
         /// <param name="dayOfWeek">The day of week in which the schedule will be activated.</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
         /// <param name="minute">The minute in which the schedule will be activated (0-59).</param>
-        public static CrontabSchedule Weekly(DayOfWeek dayOfWeek, int hour, int minute)
+        public static string Weekly(DayOfWeek dayOfWeek, int hour, int minute)
         {
-            return Parse(String.Format("{0} {1} * * {2}", minute, hour, (int) dayOfWeek));
+            return String.Format("{0} {1} * * {2}", minute, hour, (int) dayOfWeek);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every month at 00:00 UTC of the first day of month.
+        /// Returns cron expression that fires every month at 00:00 UTC of the first
+        /// day of month.
         /// </summary>
-        public static CrontabSchedule Monthly()
+        public static string Monthly()
         {
             return Monthly(day: 1);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every month at 00:00 UTC of the specified day of month.
+        /// Returns cron expression that fires every month at 00:00 UTC of the specified
+        /// day of month.
         /// </summary>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
-        public static CrontabSchedule Monthly(int day)
+        public static string Monthly(int day)
         {
             return Monthly(day, hour: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every month at the first minute of the specified day
-        /// of month and hour (int UTC).
+        /// Returns cron expression that fires every month at the first minute of the
+        /// specified day of month and hour in UTC.
         /// </summary>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
-        public static CrontabSchedule Monthly(int day, int hour)
+        public static string Monthly(int day, int hour)
         {
             return Monthly(day, hour, minute: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every month at the specified day of month, hour and minute (in UTC).
+        /// Returns cron expression that fires every month at the specified day of month,
+        /// hour and minute in UTC.
         /// </summary>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
         /// <param name="minute">The minute in which the schedule will be activated (0-59).</param>
-        public static CrontabSchedule Monthly(int day, int hour, int minute)
+        public static string Monthly(int day, int hour, int minute)
         {
-            return Parse(String.Format("{0} {1} {2} * *", minute, hour, day));
+            return String.Format("{0} {1} {2} * *", minute, hour, day);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every year on Jan, 1st at 00:00 UTC.
+        /// Returns cron expression that fires every year on Jan, 1st at 00:00 UTC.
         /// </summary>
-        public static CrontabSchedule Yearly()
+        public static string Yearly()
         {
             return Yearly(month: 1);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every year in the first day at 00:00 UTC of the specified 
-        /// month.
+        /// Returns cron expression that fires every year in the first day at 00:00 UTC
+        /// of the specified month.
         /// </summary>
         /// <param name="month">The month in which the schedule will be activated (1-12).</param>
-        public static CrontabSchedule Yearly(int month)
+        public static string Yearly(int month)
         {
             return Yearly(month, day: 1);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every year at 00:00 of the specified month and day (in UTC).
+        /// Returns cron expression that fires every year at 00:00 UTC of the specified
+        /// month and day of month.
         /// </summary>
         /// <param name="month">The month in which the schedule will be activated (1-12).</param>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
-        public static CrontabSchedule Yearly(int month, int day)
+        public static string Yearly(int month, int day)
         {
             return Yearly(month, day, hour: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every year in the first minute of the specified month, 
-        /// day and hour (in UTC).
+        /// Returns cron expression that fires every year at the first minute of the
+        /// specified month, day and hour in UTC.
         /// </summary>
         /// <param name="month">The month in which the schedule will be activated (1-12).</param>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
-        public static CrontabSchedule Yearly(int month, int day, int hour)
+        public static string Yearly(int month, int day, int hour)
         {
             return Yearly(month, day, hour, minute: 0);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class scheduled
-        /// to be activated every year in the specified month, day, hour and minute (in UTC).
+        /// Returns cron expression that fires every year at the specified month, day,
+        /// hour and minute in UTC.
         /// </summary>
         /// <param name="month">The month in which the schedule will be activated (1-12).</param>
         /// <param name="day">The day of month in which the schedule will be activated (1-31).</param>
         /// <param name="hour">The hour in which the schedule will be activated (0-23).</param>
         /// <param name="minute">The minute in which the schedule will be activated (0-59).</param>
-        public static CrontabSchedule Yearly(int month, int day, int hour, int minute)
+        public static string Yearly(int month, int day, int hour, int minute)
         {
-            return Parse(String.Format("{0} {1} {2} {3} *", minute, hour, day, month));
+            return String.Format("{0} {1} {2} {3} *", minute, hour, day, month);
         }
 
         /// <summary>
-        /// Returns an instance of the <see cref="CrontabSchedule"/> class initialized
-        /// with the given CRON expression.
+        /// Returns the given cron expression.
         /// </summary>
         /// <param name="expression">CRON expression (for example, "0 12 * */2").</param>
-        public static CrontabSchedule Parse(string expression)
+        public static string Expression(string expression)
         {
-            return CrontabSchedule.Parse(expression);
+            return expression;
         }
     }
 }
