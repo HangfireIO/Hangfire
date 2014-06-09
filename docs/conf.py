0367eb006f0cfdd22517585ba5d6a4f7a6b59f0e
odinserj <odinserj@gmail.com>
2014-06-10 01:17:56 +0600

    Apply CC BY 4.0 license for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HangFire'
-copyright = u'2014, Sergey Odinokov'
+copyright = u'2014, Sergey Odinokov. Documentation is licensed under the CC BY 4.0'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
