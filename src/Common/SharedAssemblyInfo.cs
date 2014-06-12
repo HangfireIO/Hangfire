e91c45e4511f32e849015ea96fce08c2c958a237
Sergey Odinokov <odinserj@gmail.com>
2014-06-12 13:21:57 +0400

    Bump version to 0.9.1

--- src/Common/SharedAssemblyInfo.cs
+++ src/Common/SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyInformationalVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
