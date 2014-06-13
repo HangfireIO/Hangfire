47ee59f74d7ac4ebb1972180042d9be197cfd761
odinserj <odinserj@gmail.com>
2014-06-13 15:15:19 +0600

    Enable unit testing during Travis CI build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export EnableNuGetPackageRestore="true"
 xbuild HangFire.Mono.sln
+mono --runtime=v4.0 tools/xunit/xunit.console.clr4.x86.exe tests/HangFire.Core.Tests/bin/Debug/HangFire.Core.Tests.dll
