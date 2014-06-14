aed5330ebec64d783780ddabc8cfa3956f4c1e40
Sergey Odinokov <odinserj@gmail.com>
2014-06-14 16:16:59 +0400

    Add DashboardMiddleware class

---
+++ src/HangFire.Core/Dashboard/DashboardMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace HangFire.Dashboard
+{
+    public class DashboardMiddleware : OwinMiddleware
+    {
+        public DashboardMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
