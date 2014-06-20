4b4460d6026b3a2cbe93dccea5a8d3f1f2c770fe
Denny Ferrassoli <denny.ferrassoli@gmail.com>
2014-06-20 16:13:59 -0700

    Add project targets for build

--- src/HangFire.SqlServer.RabbitMq/Properties/AssemblyInfo.cs
+++ src/HangFire.SqlServer.RabbitMq/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("HangFire RabbitMQ job queue for SQL Server storage implementation")]
 [assembly: Guid("03092b5c-0dfc-4c6c-8422-556bd1cb291e")]
 
-[assembly: InternalsVisibleTo("HangFire.SqlServer.RabbitMQ.Tests")]
+[assembly: InternalsVisibleTo("HangFire.SqlServer.RabbitMq.Tests")]
 // Allow the generation of mocks for internal types
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
