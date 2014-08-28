026604b8f290608125213d0421867a9cd4c5c390
Excommunicated <matthew.sheeley@gmail.com>
2014-08-28 08:00:43 -0500

    Add Latency Filter Attribute To Delete job if timeout is exceeded

---
+++ src/Hangfire.Core/DeleteOnLatencyTimeoutAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Common.Logging;
+using Hangfire.Common;
+using Hangfire.States;
+using Hangfire.Storage;
+
+namespace Hangfire
+{
+    public class DeleteOnLatencyTimeoutAttribute : JobFilterAttribute, IElectStateFilter
+    {
+        private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+        private int _timeoutInSeconds;
+        private const int DefaultTimeoutInSeconds = 300;
+
+
+        public bool LogEvents { get; set; }
+        public int TimeoutInSeconds
+        {
+            get { return _timeoutInSeconds; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "TimeoutInSeconds value must be equal or greater that zero.");
+                }
+                _timeoutInSeconds = value;
+            }
+        }
+
+        public DeleteOnLatencyTimeoutAttribute()
+        {
+            TimeoutInSeconds = DefaultTimeoutInSeconds;
+            LogEvents = true;
+        }
+
+        public void OnStateElection(ElectStateContext context)
+        {
+            var state = context.CandidateState as ProcessingState;
+            if (state == null)
+            {
+                //this filter only accepts Processing
+                return;
+            }
+
+            var elapsedTime = DateTime.UtcNow - context.CreatedAt;
+
+            if (elapsedTime.TotalSeconds > TimeoutInSeconds)
+            {
+                context.CandidateState = new DeletedState()
+                {
+                    Reason = String.Format("Job has exceeded latency timeout of {0} seconds", TimeoutInSeconds)
+                };
+
+                if (LogEvents)
+                {
+                    Logger.WarnFormat(
+                        "Failed to process the job '{0}': Job has exceeded latency timeout of {1} seconds",
+                        context.JobId,
+                        TimeoutInSeconds
+                        );
+                }
+            }
+        }
+
+    }
+}
---
+++ tests/Hangfire.Core.Tests/DeleteOnLatencyAttributeFacts.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Hangfire.States;
+using Hangfire.Storage;
+using Moq;
+using NUnit.Framework;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Hangfire.Core.Tests
+{
+    public class DeleteOnLatencyAttributeFacts
+    {
+        private const string JobId = "id";
+
+        private readonly ProcessingState _state;
+        private readonly Mock<IStorageConnection> _connection;
+        private readonly ElectStateContextMock _context;
+
+        public DeleteOnLatencyAttributeFacts()
+        {
+            _state = new ProcessingState("Default",1);
+            _connection = new Mock<IStorageConnection>();
+
+            _context = new ElectStateContextMock();
+            _context.StateContextValue.JobIdValue = JobId;
+            _context.StateContextValue.ConnectionValue = _connection;
+            _context.CandidateStateValue = _state;
+            _context.StateContextValue.CreatedAtValue = DateTime.UtcNow;
+
+        }
+
+        [Fact]
+        public void Ctor_SetsDefaultLatencyTimeoutTo300SecodsByDefault()
+        {
+            var filter = new DeleteOnLatencyTimeoutAttribute();
+            Assert.Equal(300,filter.TimeoutInSeconds);
+        }
+
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenTimeoutInSecondsValueIsNegative()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => CreateFilter(-1));
+        }
+
+        [Fact]
+        public void OnStateElection_DoesNotChangeAnything_IfTimeoutNotExceeded()
+        {
+            var filter = CreateFilter();
+            filter.OnStateElection(_context.Object);
+
+            Assert.IsType<ProcessingState>(_context.Object.CandidateState);
+        }
+
+        [Fact]
+        public void OnStateElection_ChangesToDeleted_IfTimeoutExceeded()
+        {
+            var filter = CreateFilter(0);
+            filter.OnStateElection(_context.Object);
+
+            Assert.IsType<DeletedState>(_context.Object.CandidateState);
+        }
+
+        private static DeleteOnLatencyTimeoutAttribute CreateFilter(int delayInSeconds = 100)
+        {
+            return new DeleteOnLatencyTimeoutAttribute {TimeoutInSeconds = delayInSeconds};
+        }
+    }
+}
