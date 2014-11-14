cb68b709737c344a8e33ca1d19610a59b59eb63d
RicksonByArmbar@users.noreply.github.com <RicksonByArmbar@users.noreply.github.com>
2014-11-14 10:12:59 +0000

    Reverted formatting changes

--- src/Hangfire.Core/Common/Job.cs
+++ src/Hangfire.Core/Common/Job.cs
@@ -13,7 +13,6 @@
 // 
 // You should have received a copy of the GNU Lesser General Public 
 // License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
