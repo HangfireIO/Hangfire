c2ba3439cb82d799e739ff96a61a6a223481f1e6
RicksonByArmbar@users.noreply.github.com <RicksonByArmbar@users.noreply.github.com>
2014-11-14 11:03:10 +0000

    Revert "Reverted formatting changes"
    This reverts commit cb68b709737c344a8e33ca1d19610a59b59eb63d.

--- src/Hangfire.Core/Common/Job.cs
+++ src/Hangfire.Core/Common/Job.cs
@@ -13,6 +13,7 @@
 // 
 // You should have received a copy of the GNU Lesser General Public 
 // License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
