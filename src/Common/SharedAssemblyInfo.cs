0bd30b71d7cd20e6015868757ed5a960611797a2
Sergey Odinokov <odinserj@gmail.com>
2014-12-03 21:11:19 +0300

    Bump version to 1.3.0

--- src/Common/SharedAssemblyInfo.cs
+++ src/Common/SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyInformationalVersion("1.2.2")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
