c4496469731233caf56b20372f07f12769258ca8
Sergey Odinokov <odinserj@gmail.com>
2014-12-05 04:19:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-export EnableNuGetPackageRestore="true"
+set -e
+mono --runtime=v4.0 .nuget/NuGet.exe install .nuget/packages.config -OutputDirectory packages
+mono --runtime=v4.0 .nuget/NuGet.exe restore Hangfire.Mono.sln
 xbuild Hangfire.Mono.sln /verbosity:minimal
-# mono --runtime=v4.0 tools/xunit/xunit.console.clr4.x86.exe tests/Hangfire.Core.Tests/bin/Debug/Hangfire.Core.Tests.dll
-# mono --runtime=v4.0 tools/xunit/xunit.console.clr4.x86.exe tests/Hangfire.SqlServer.RabbitMq.Tests/bin/Debug/Hangfire.SqlServer.RabbitMq.Tests.dll
+mono --runtime=v4.0 ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe tests/Hangfire.Core.Tests/bin/Debug/Hangfire.Core.Tests.dll
+mono --runtime=v4.0 ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe tests/Hangfire.SqlServer.RabbitMq.Tests/bin/Debug/Hangfire.SqlServer.RabbitMq.Tests.dll
