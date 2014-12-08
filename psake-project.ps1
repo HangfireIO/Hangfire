e744bd624350ea2b1016a08ae8b62020f4de88da
Sergey Odinokov <odinserj@gmail.com>
2014-12-08 19:04:29 +0300

    Ignore Dapper.pdb file for ILMerge

--- psake-project.ps1
+++ psake-project.ps1
@@ -13,8 +13,9 @@ Task Test -Depends Compile -Description "Run unit and integration tests." {
 }
 
 Task Merge -Depends Test -Description "Run ILMerge /internalize to merge assemblies." {
-    # Remove `NCrontab.pdb` file to be able to prepare a symbol package for Hangfire.Core.
+    # Remove `*.pdb` file to be able to prepare NuGet symbol packages.
     Remove-Item ((Get-SrcOutputDir "Hangfire.Core") + "\NCrontab.pdb")
+    Remove-Item ((Get-SrcOutputDir "Hangfire.SqlServer") + "\Dapper.pdb")
 
     Merge-Assembly "Hangfire.Core" @("NCrontab", "CronExpressionDescriptor", "Microsoft.Owin")
     Merge-Assembly "Hangfire.SqlServer" @("Dapper")
