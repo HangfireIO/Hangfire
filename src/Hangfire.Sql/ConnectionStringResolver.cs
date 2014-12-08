3078949475b6b8db48b902064eb60e5d775f7934
andrecarlucci <andrecarlucci@gmail.com>
2014-12-08 17:06:36 -0200

    Resharper settings via Solution team-shared

--- src/Hangfire.Sql/ConnectionStringResolver.cs
+++ src/Hangfire.Sql/ConnectionStringResolver.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿// // This file is part of Hangfire.
+// // Copyright © 2013-2014 Sergey Odinokov.
+// // 
+// // Hangfire is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU Lesser General Public License as 
+// // published by the Free Software Foundation, either version 3 
+// // of the License, or any later version.
+// // 
+// // Hangfire is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU Lesser General Public License for more details.
+// // 
+// // You should have received a copy of the GNU Lesser General Public 
+// // License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Configuration;
 
-namespace Hangfire.Sql {
-    public static class ConnectionStringResolver {
-        public static string GetConnectionString(string nameOrConnectionString) {
-            if (IsConnectionString(nameOrConnectionString)) {
+namespace Hangfire.Sql
+{
+    public static class ConnectionStringResolver
+    {
+        public static string GetConnectionString(string nameOrConnectionString)
+        {
+            if (IsConnectionString(nameOrConnectionString))
+            {
                 return nameOrConnectionString;
             }
-            if (IsConnectionStringInConfiguration(nameOrConnectionString)) {
+            if (IsConnectionStringInConfiguration(nameOrConnectionString))
+            {
                 return ConfigurationManager.ConnectionStrings[nameOrConnectionString].ConnectionString;
             }
             throw new ArgumentException(
@@ -15,11 +36,13 @@ namespace Hangfire.Sql {
                     nameOrConnectionString));
         }
 
-        private static bool IsConnectionString(string nameOrConnectionString) {
+        private static bool IsConnectionString(string nameOrConnectionString)
+        {
             return nameOrConnectionString.Contains(";");
         }
 
-        private static bool IsConnectionStringInConfiguration(string connectionStringName) {
+        private static bool IsConnectionStringInConfiguration(string connectionStringName)
+        {
             var connectionStringSetting = ConfigurationManager.ConnectionStrings[connectionStringName];
 
             return connectionStringSetting != null;
