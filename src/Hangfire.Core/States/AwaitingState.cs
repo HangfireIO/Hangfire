997b43578eac823c837277da5a3412186510a43c
Sergey Odinokov <odinserj@gmail.com>
2015-08-17 20:42:39 +0300

    Start commenting the AwaitingState class

--- src/Hangfire.Core/States/AwaitingState.cs
+++ src/Hangfire.Core/States/AwaitingState.cs
@@ -29,26 +29,62 @@ namespace Hangfire.States
     /// <see cref="EnqueuedState"/> by the <see cref="ContinuationsSupportAttribute"/>
     /// filter.
     /// </summary>
+    /// 
+    /// <remarks>TODO</remarks>
     public class AwaitingState : IState
     {
         private static readonly TimeSpan DefaultExpiration = TimeSpan.FromDays(365);
-        public static string StateName = "Awaiting";
 
+        /// <summary>
+        /// Represents the name of the <i>Awaiting</i> state. This field is read-only.
+        /// </summary>
+        /// <remarks>
+        /// The value of this field is <c>"Awaiting"</c>.
+        /// </remarks>
+        public static readonly string StateName = "Awaiting";
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwaitingState"/> class with
+        /// the specified parent background job id and with an instance of the 
+        /// <see cref="EnqueuedState"/> class as a next state.
+        /// </summary>
+        /// <param name="parentId"></param>
         public AwaitingState(string parentId)
             : this(parentId, new EnqueuedState())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwaitingState"/> class with
+        /// the specified parent job id and next state.
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <param name="nextState"></param>
         public AwaitingState(string parentId, IState nextState)
             : this(parentId, nextState, JobContinuationOptions.OnAnyFinishedState)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwaitingState"/> class with
+        /// the given <i>options</i> along with other parameters.
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <param name="nextState"></param>
+        /// <param name="options"></param>
         public AwaitingState(string parentId, IState nextState, JobContinuationOptions options)
             : this(parentId, nextState, options, DefaultExpiration)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwaitingState"/> class with
+        /// the specified <i>expiration time</i> along with other parameters.
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <param name="nextState"></param>
+        /// <param name="options"></param>
+        /// <param name="expiration"></param>
         [JsonConstructor]
         public AwaitingState(
             [NotNull] string parentId,
@@ -66,18 +102,91 @@ namespace Hangfire.States
             Expiration = expiration;
         }
 
+        /// <summary>
+        /// Gets the identifier of a parent background job.
+        /// </summary>
         public string ParentId { get; private set; }
+
+        /// <summary>
+        /// Gets the next state, to which a background job will be moved.
+        /// </summary>
         public IState NextState { get; private set; }
 
+        /// <summary>
+        /// Gets the continuation options associated with the current state.
+        /// </summary>
         public JobContinuationOptions Options { get; private set; }
+
+        /// <summary>
+        /// Gets the expiration time of a background job continuation.
+        /// </summary>
         public TimeSpan Expiration { get; private set; }
 
+        /// <inheritdoc />
+        /// <remarks>
+        /// Always equals to <see cref="StateName"/> for the <see cref="AwaitingState"/>.
+        /// Please see the remarks section of the <see cref="IState.Name">IState.Name</see>
+        /// article for the details.
+        /// </remarks>
         public string Name { get { return StateName; } }
+
+        /// <inheritdoc />
         public string Reason { get; set; }
 
+        /// <inheritdoc />
+        /// <remarks>
+        /// Always returns <see langword="false"/> for the <see cref="AwaitingState"/>.
+        /// Please refer to the <see cref="IState.IsFinal">IState.IsFinal</see> documentation
+        /// for the details.
+        /// </remarks>
         public bool IsFinal { get { return false; } }
+
+        /// <inheritdoc />
+        /// <remarks>
+        /// Always returns <see langword="false" /> for the <see cref="AwaitingState"/>.
+        /// Please see the description of this property in the
+        /// <see cref="IState.IgnoreJobLoadException">IState.IgnoreJobLoadException</see>
+        /// article.
+        /// </remarks>
         public bool IgnoreJobLoadException { get { return false; } }
 
+        /// <inheritdoc />
+        /// <remarks>
+        /// <para>Returning dictionary contains the following keys. You can obtain 
+        /// the state data by using the <see cref="IStorageConnection.GetStateData"/>
+        /// method.</para>
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Key</term>
+        ///         <term>Type</term>
+        ///         <term>Deserialize Method</term>
+        ///         <description>Notes</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term><c>ParentId</c></term>
+        ///         <term><see cref="string"/></term>
+        ///         <term><i>Not required</i></term>
+        ///         <description>Please see the <see cref="ParentId"/> property.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term><c>NextState</c></term>
+        ///         <term><see cref="IState"/></term>
+        ///         <term>
+        ///             <see cref="JsonConvert.DeserializeObject(string, JsonSerializerSettings)"/> with 
+        ///             <see cref="TypeNameHandling.Objects"/>
+        ///         </term>
+        ///         <description>Please see the <see cref="NextState"/> property.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term><c>Options</c></term>
+        ///         <term><see cref="JobContinuationOptions"/></term>
+        ///         <term>
+        ///             <see cref="Enum.Parse(Type, string)"/> with <see cref="JobContinuationOptions"/>
+        ///         </term>
+        ///         <description>Please see the <see cref="Options"/> property.</description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
         public Dictionary<string, string> SerializeData()
         {
             return new Dictionary<string, string>
@@ -101,6 +210,7 @@ namespace Hangfire.States
                 transaction.RemoveFromSet("awaiting", context.JobId);
             }
 
+            // ReSharper disable once MemberHidesStaticFromOuterClass
             public string StateName
             {
                 get { return AwaitingState.StateName; }
