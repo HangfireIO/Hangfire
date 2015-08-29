23fd4d18a46ed4e772d2f496f43f3a0beef9950c
Sergey Odinokov <odinserj@gmail.com>
2015-08-29 17:30:02 +0300

    Fix return value comment in xmldoc for IStateChangeProcess

--- src/Hangfire.Core/States/IStateChangeProcess.cs
+++ src/Hangfire.Core/States/IStateChangeProcess.cs
@@ -22,7 +22,7 @@ namespace Hangfire.States
         /// Attempts to change the state of a job, respecting any applicable job filters and state handlers
         /// <remarks>Also ensures that the job data can be loaded for this job</remarks>
         /// </summary>
-        /// <returns><c>Null</c> if a constraint has failed or if the job data could not be loaded, otherwise the final applied state</returns>
+        /// <returns><c>Null</c> if a constraint has failed, otherwise the final applied state</returns>
         IState ChangeState(StateChangeContext context);
     }
 }
