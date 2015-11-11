be7bd3693c59102f0c1ab35a6b493c489531b8f8
Simon Harriyott <simon@harriyott.com>
2015-11-11 10:15:33 +0000

    Replaced merge with update / insert in job parameter

--- src/Hangfire.SqlServer/SqlServerConnection.cs
+++ src/Hangfire.SqlServer/SqlServerConnection.cs
@@ -202,12 +202,13 @@ where j.Id = @jobId", _storage.GetSchemaName());
             _storage.UseConnection(connection =>
             {
                 connection.Execute(
-                    string.Format(@";merge [{0}].JobParameter with (holdlock) as Target "
-                    + @"using (VALUES (@jobId, @name, @value)) as Source (JobId, Name, Value) "
-                    + @"on Target.JobId = Source.JobId AND Target.Name = Source.Name "
-                    + @"when matched then update set Value = Source.Value "
-                    +
-                    @"when not matched then insert (JobId, Name, Value) values (Source.JobId, Source.Name, Source.Value);", _storage.GetSchemaName()),
+                    string.Format(@";UPDATE [{0}].JobParameter "
+                    + @"SET [Value] = @value "
+                    + @"WHERE JobId = @jobId AND [Name] = @name; "
+                    + @"IF @@ROWCOUNT = 0 "
+                    + @"INSERT INTO [HangFire].JobParameter (JobId, Name, Value) "
+                    + @"VALUES(@jobId, @name, @value);",
+                    _storage.GetSchemaName()),
                     new { jobId = id, name, value });
             });
         }
