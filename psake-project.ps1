30a4d3f564863e4d590903ccd352398ecf8dea10
Sergey Odinokov <odinserj@gmail.com>
2016-01-21 16:46:33 +0300

    Remove Hangfire.SqlServer.RabbitMQ project
    Closes #498, @danyferra will maintain this project

--- psake-project.ps1
+++ psake-project.ps1
@@ -32,7 +32,6 @@ Task Collect -Depends Merge -Description "Copy all artifacts to the build folder
     Collect-Assembly "Hangfire.Core" "Net45"
     Collect-Assembly "Hangfire.SqlServer" "Net45"
     Collect-Assembly "Hangfire.SqlServer.Msmq" "Net45"
-    Collect-Assembly "Hangfire.SqlServer.RabbitMq" "Net45"
     
     Collect-Content "content\readme.txt"
     Collect-Tool "src\Hangfire.SqlServer\DefaultInstall.sql"
@@ -53,7 +52,6 @@ Task Pack -Depends Collect -Description "Create NuGet packages and archive files
     Create-Package "Hangfire.Core" $version
     Create-Package "Hangfire.SqlServer" $version
     Create-Package "Hangfire.SqlServer.Msmq" $version
-    Create-Package "Hangfire.SqlServer.RabbitMq" $version
 }
 
 function Run-OpenCover($assembly) {
