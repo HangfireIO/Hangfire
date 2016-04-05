91489ec637e8435f5d9ccf6c058103f6956366eb
Sergey Odinokov <odinserj@gmail.com>
2016-04-05 14:39:37 +0300

    Add license headers for new files

--- src/Hangfire.Core/IRecurringJobManager.cs
+++ src/Hangfire.Core/IRecurringJobManager.cs
@@ -1,15 +1,27 @@
-﻿using Hangfire.Common;
+﻿// This file is part of Hangfire.
+// Copyright © 2016 Sergey Odinokov.
+// 
+// Hangfire is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as 
+// published by the Free Software Foundation, either version 3 
+// of the License, or any later version.
+// 
+// Hangfire is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public 
+// License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
+
+using Hangfire.Common;
 
 namespace Hangfire
 {
     public interface IRecurringJobManager
     {
-        void AddOrUpdate(string recurringJobId, Job job, string cronExpression);
-
         void AddOrUpdate(string recurringJobId, Job job, string cronExpression, RecurringJobOptions options);
-
         void Trigger(string recurringJobId);
-
         void RemoveIfExists(string recurringJobId);
     }
 }
