681e79613d92ab5b7ed35b1c48afc8098e9c3b8d
Sergey Odinokov <odinserj@gmail.com>
2016-07-15 14:21:44 +0300

    Use shorthands for determining IP addresses in ASP.NET Core

--- src/Hangfire.AspNetCore/Dashboard/AspNetCoreDashboardRequest.cs
+++ src/Hangfire.AspNetCore/Dashboard/AspNetCoreDashboardRequest.cs
@@ -19,7 +19,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hangfire.Annotations;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 
 namespace Hangfire.Dashboard
 {
@@ -30,19 +29,14 @@ namespace Hangfire.Dashboard
         public AspNetCoreDashboardRequest([NotNull] HttpContext context)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
-
             _context = context;
-
-            var connection = _context.Features.Get<IHttpConnectionFeature>();
-            LocalIpAddress = connection.LocalIpAddress.ToString();
-            RemoteIpAddress = connection.RemoteIpAddress.ToString();
         }
 
         public override string Method => _context.Request.Method;
         public override string Path => _context.Request.Path.Value;
         public override string PathBase => _context.Request.PathBase.Value;
-        public override string LocalIpAddress { get; }
-        public override string RemoteIpAddress { get; }
+        public override string LocalIpAddress => _context.Connection.LocalIpAddress.ToString();
+        public override string RemoteIpAddress => _context.Connection.RemoteIpAddress.ToString();
         public override string GetQuery(string key) => _context.Request.Query[key];
 
         public override async Task<IList<string>> GetFormValuesAsync(string key)
