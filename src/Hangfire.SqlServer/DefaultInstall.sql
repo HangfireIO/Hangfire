e1ce2cb4f71c38ba0f9d2ce1231e65361e354c13
Sergey Odinokov <odinserj@gmail.com>
2016-08-04 12:12:59 +0300

    Acquire exclusive lock before starting db migration
    Fixes #636

--- src/Hangfire.SqlServer/DefaultInstall.sql
+++ src/Hangfire.SqlServer/DefaultInstall.sql
@@ -24,6 +24,9 @@ PRINT 'Installing Hangfire SQL objects...';
 BEGIN TRANSACTION;
 BEGIN TRY;
 
+-- Acquire exclusive lock to prevent deadlocks caused by schema creation / version update
+DECLARE @SchemaLockResult INT;
+EXEC @SchemaLockResult = sp_getapplock @Resource = 'HangFire:SchemaLock', @LockMode = 'Exclusive'
 
 -- Create the database schema if it doesn't exists
 IF NOT EXISTS (SELECT [schema_id] FROM [sys].[schemas] WHERE [name] = 'HangFire')
