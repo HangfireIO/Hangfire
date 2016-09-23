78ec02d0275eef80fd92dbd7147ffe2c63dfd0b4
Alexey Skalozub <pieceofsummer@gmail.com>
2016-09-24 02:05:31 +0300

    Rewrite JobActivatorScope using AsyncLocal<>, add some tests

--- src/Hangfire.Core/JobActivatorScope.cs
+++ src/Hangfire.Core/JobActivatorScope.cs
@@ -19,36 +19,82 @@ using System.Threading;
 
 namespace Hangfire
 {
+    /// <summary>
+    /// Base class for <see cref="JobActivator"/> scopes.
+    /// </summary>
     public abstract class JobActivatorScope : IDisposable
     {
         // ReSharper disable once InconsistentNaming
-        private static readonly ThreadLocal<JobActivatorScope> _current
-            = new ThreadLocal<JobActivatorScope>(trackAllValues: false);
+        private static readonly AsyncLocal<JobActivatorScope> _current
+            = new AsyncLocal<JobActivatorScope>();
+
+        private readonly JobActivatorScope _parent;
+        private bool _disposed;
 
         protected JobActivatorScope()
         {
+            _parent = _current.Value;
             _current.Value = this;
+            _disposed = false;
         }
 
+        /// <summary>
+        /// Returns <see cref="JobActivatorScope"/> the code is running in.
+        /// </summary>
         public static JobActivatorScope Current => _current.Value;
 
-        public object InnerScope { get; set; }
+        /// <summary>
+        /// Returns an enclosing <see cref="JobActivatorScope"/> for this scope.
+        /// </summary>
+        internal JobActivatorScope ParentScope => _parent;
 
+        /// <summary>
+        /// Resolve and activate an instance of <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">Class or interface type to activate</param>
+        /// <returns>
+        /// Instance of a <paramref name="type"/>, 
+        /// or <c>null</c> if the specified type cannot be activated
+        /// </returns>
         public abstract object Resolve(Type type);
 
-        public virtual void DisposeScope()
+        /// <summary>
+        /// Called when current <see cref="JobActivatorScope"/> is about to be disposed.
+        /// Subclasses may override this to perform some cleanup before leaving scope.
+        /// </summary>
+        protected virtual void DisposeScope()
         {
+        }
+
+        /// <summary>
+        /// Throws <seealso cref="ObjectDisposedException"/> if the current scope is already disposed.
+        /// First thing for subclasses to check in their <seealso cref="Resolve(Type)"/> implementations!
+        /// </summary>
+        protected void AssertNotDisposed()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JobActivatorScope));
         }
 
         public void Dispose()
         {
+            if (_disposed) return;
+
+            // immediately mark as disposed, so Dispose() will never be reentered, 
+            // even if mistakenly called from within DisposeScope() override
+            _disposed = true;
+
+            if (_current.Value != this)
+                throw new InvalidOperationException("Messed up JobActivatorScope dispose order");
+
             try
             {
                 DisposeScope();
             }
             finally
             {
-                _current.Value = null;
+                // restore previous scope
+                _current.Value = _parent;
             }
         }
     }
