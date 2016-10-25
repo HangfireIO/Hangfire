80b384ca731d77249959624df08c26cde5673b79
Sergey Odinokov <odinserj@gmail.com>
2016-10-25 20:15:43 +0300

    Fractial milliseconds now formatted correctly in Dashboard
    Fixes #708

---
+++ tests/Hangfire.Core.Tests/Dashboard/HtmlHelperFacts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Hangfire.Dashboard;
+using Moq;
+using Xunit;
+
+namespace Hangfire.Core.Tests.Dashboard
+{
+    public class HtmlHelperFacts
+    {
+        private readonly Mock<RazorPage> _page;
+
+        public HtmlHelperFacts()
+        {
+            _page = new Mock<RazorPage>();
+        }
+
+        [Fact]
+        public void ToHumanDuration_FormatsFractionalSeconds()
+        {
+            var helper = CreateHelper();
+            var result = helper.ToHumanDuration(TimeSpan.FromSeconds(1.087));
+            Assert.Equal("+1.087s", result);
+        }
+
+        private HtmlHelper CreateHelper()
+        {
+            return new HtmlHelper(_page.Object);
+        }
+    }
+}
