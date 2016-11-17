903b4cd0c41e14abf5a9871f9b406d785f06feed
aidmsu <aidmsu@gmail.com>
2016-11-17 17:12:59 +0300

    Add tests covered ServerWatchdogOptions class

---
+++ tests/Hangfire.Core.Tests/Obsolete/ServerWatchdogOptionsFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Hangfire.Server;
+using Xunit;
+
+namespace Hangfire.Core.Tests.Obsolete
+{
+    public class ServerWatchdogOptionsFacts
+    {
+        [Fact]
+        public void Ctor_InitializeProperties_WithCorrectValues()
+        {
+            var options = new ServerWatchdogOptions();
+
+            Assert.True(options.CheckInterval >= TimeSpan.Zero || options.CheckInterval == Timeout.InfiniteTimeSpan);
+            Assert.True(options.ServerTimeout > TimeSpan.Zero && options.ServerTimeout <= TimeSpan.FromHours(24));
+        }
+
+        [Fact]
+        public void ServerTimeout_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = new ServerWatchdogOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerTimeout = TimeSpan.FromHours(25));
+        }
+    }
+}
