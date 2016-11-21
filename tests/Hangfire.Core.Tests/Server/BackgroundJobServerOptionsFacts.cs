df42c3ebb1592c25f5926ecdc69dc2148f4d052c
Andrey Dorokhov <aidmsu@gmail.com>
2016-11-21 15:44:57 +0300

    Validate BackgroundJobServerOptions properties (#733)
    * Throw exception if server timeout is greater than 24 hours
    * Refactoring. Avoid constant duplication. Change ArgumentException to ArgumentOutOfRangeException
    * Small refactoring. Fix some stupid bugs of previous commit
    * Add tests that cover ServerWatchdogOptions class
    * Add tests that cover some interval and timeout properties of BackgroundJobServerOptionsFacts class. Add max value constraint for these properties
    * SchedulePollingInterval can't be inifinite

--- tests/Hangfire.Core.Tests/Server/BackgroundJobServerOptionsFacts.cs
+++ tests/Hangfire.Core.Tests/Server/BackgroundJobServerOptionsFacts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Hangfire.States;
 using Xunit;
 
@@ -13,8 +14,11 @@ namespace Hangfire.Core.Tests.Server
 
             Assert.Equal(Math.Min(Environment.ProcessorCount * 5, 20), options.WorkerCount);
             Assert.Equal(EnqueuedState.DefaultQueue, options.Queues[0]);
-            Assert.True(options.ShutdownTimeout > TimeSpan.Zero);
-            Assert.True(options.SchedulePollingInterval > TimeSpan.Zero);
+            Assert.Equal(TimeSpan.FromSeconds(15), options.ShutdownTimeout);
+            Assert.Equal(TimeSpan.FromSeconds(15), options.SchedulePollingInterval);
+            Assert.Equal(TimeSpan.FromMinutes(5), options.ServerTimeout);
+            Assert.Equal(TimeSpan.FromMinutes(5), options.ServerCheckInterval);
+            Assert.Equal(TimeSpan.FromSeconds(30), options.HeartbeatInterval);
         }
 
         [Fact]
@@ -51,6 +55,142 @@ namespace Hangfire.Core.Tests.Server
 
             Assert.Throws<ArgumentException>(
                 () => options.Queues = new string[0]);
+        }
+
+        [Fact]
+        public void ServerTimeout_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = CreateOptions();
+        
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerTimeout = TimeSpan.FromHours(25));
+        }
+
+        [Fact]
+        public void ServerTimeout_ThrowsAnException_WhenValueIsNegative()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerTimeout = TimeSpan.FromHours(-1));
+        }
+
+        [Fact]
+        public void ServerTimeout_ThrowsAnException_WhenValueIsInfinite()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerTimeout = Timeout.InfiniteTimeSpan);
+        }
+
+        [Fact]
+        public void ServerCheckInterval_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerCheckInterval = TimeSpan.FromHours(25));
+        }
+
+        [Fact]
+        public void ServerCheckInterval_ThrowsAnException_WhenValueIsNegative()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerCheckInterval = TimeSpan.FromHours(-1));
+        }
+
+        [Fact]
+        public void ServerCheckInterval_ThrowsAnException_WhenValueIsInfinite()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ServerCheckInterval = Timeout.InfiniteTimeSpan);
+        }
+
+        [Fact]
+        public void HeartbeatInterval_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.HeartbeatInterval = TimeSpan.FromHours(25));
+        }
+
+        [Fact]
+        public void HeartbeatInterval_ThrowsAnException_WhenValueIsNegative()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.HeartbeatInterval = TimeSpan.FromHours(-1));
+        }
+
+        [Fact]
+        public void HeartbeatInterval_ThrowsAnException_WhenValueIsInfinite()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.HeartbeatInterval = Timeout.InfiniteTimeSpan);
+        }
+
+        [Fact]
+        public void ShutdownTimeout_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ShutdownTimeout = TimeSpan.FromMilliseconds((double)Int32.MaxValue+1));
+        }
+
+        [Fact]
+        public void ShutdownTimeout_ThrowsAnException_WhenValueIsNegative()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.ShutdownTimeout = TimeSpan.FromHours(-1));
+        }
+
+        [Fact]
+        public void ShutdownTimeout_DoesNotThrowAnException_WhenValueIsInfinite()
+        {
+            var options = CreateOptions();
+
+            options.ShutdownTimeout = Timeout.InfiniteTimeSpan;
+
+            Assert.Equal(Timeout.InfiniteTimeSpan, options.ShutdownTimeout);
+        }
+
+        [Fact]
+        public void SchedulePollingInterval_ThrowsAnException_WhenValueIsTooLarge()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.SchedulePollingInterval = TimeSpan.FromMilliseconds((double)Int32.MaxValue + 1));
+        }
+
+        [Fact]
+        public void SchedulePollingInterval_ThrowsAnException_WhenValueIsNegative()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.SchedulePollingInterval = TimeSpan.FromHours(-1));
+        }
+
+        [Fact]
+        public void SchedulePollingInterval_ThrowsAnException_WhenValueIsInfinite()
+        {
+            var options = CreateOptions();
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => options.SchedulePollingInterval = Timeout.InfiniteTimeSpan);
         }
 
         private static BackgroundJobServerOptions CreateOptions()
