fa6777445fe99b680ea2a079af04dded73458ada
Sergey Odinokov <odinserj@gmail.com>
2016-12-02 15:23:46 +0300

    Add support for `bigint` type in SQL's identity columns
    The support is partial, some features of monitoring API may not support
    bigint type yet, but all the background processing correctly supports
    it.

--- src/Hangfire.SqlServer/IPersistentJobQueueMonitoringApi.cs
+++ src/Hangfire.SqlServer/IPersistentJobQueueMonitoringApi.cs
@@ -21,8 +21,13 @@ namespace Hangfire.SqlServer
     public interface IPersistentJobQueueMonitoringApi
     {
         IEnumerable<string> GetQueues();
+
+        // TODO: Change return type to `IEnumerable<long>` to support `bigint` type.
         IEnumerable<int> GetEnqueuedJobIds(string queue, int from, int perPage);
+
+        // TODO: Change return type to `IEnumerable<long>` to support `bigint` type.
         IEnumerable<int> GetFetchedJobIds(string queue, int from, int perPage);
+
         EnqueuedAndFetchedCountDto GetEnqueuedAndFetchedCount(string queue);
     }
 }
