ce464f4c09aa98f76a2aba8e6c43df7729de436d
Jesse Klaasse <Jesse.Klaasse@indicia.nl>
2017-07-11 15:02:11 +0200

    Add a test class and first test for SqlServerMonitoringApi

---
+++ tests/Hangfire.SqlServer.Tests/SqlServerMonitoringApiFacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Hangfire.SqlServer.Tests
+{
+    public class SqlServerMonitoringApiFacts
+    {
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenStorageIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new SqlServerMonitoringApi(null, null));
+
+            Assert.Equal("storage", exception.ParamName);
+        }
+    }
+}
