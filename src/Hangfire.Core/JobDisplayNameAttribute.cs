0a418258e3d5bb6e6b6653e6adf74edf11e5e43f
Alexey Skalozub <pieceofsummer@gmail.com>
2017-09-14 00:23:39 +0300

    More efficient ResourceManager caching

--- src/Hangfire.Core/JobDisplayNameAttribute.cs
+++ src/Hangfire.Core/JobDisplayNameAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Globalization;
+using System.Reflection;
 using System.Resources;
 
 namespace Hangfire
@@ -10,7 +12,8 @@ namespace Hangfire
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public sealed class JobDisplayNameAttribute : Attribute
     {
-        private readonly Lazy<ResourceManager> _resourceManager;
+        private static readonly ConcurrentDictionary<Type, ResourceManager> _resourceManagerCache
+            = new ConcurrentDictionary<Type, ResourceManager>();
 
         public JobDisplayNameAttribute(string displayName)
         {
@@ -18,7 +21,6 @@ namespace Hangfire
                 throw new ArgumentException("Display name is empty", nameof(displayName));
 
             DisplayName = displayName;
-            _resourceManager = new Lazy<ResourceManager>(() => InitResourceManager(ResourceType), true);
         }
 
         /// <summary>
@@ -33,18 +35,31 @@ namespace Hangfire
 
         private static ResourceManager InitResourceManager(Type type)
         {
-            if (type == null) return null;
-
+            var prop = type.GetTypeInfo().GetDeclaredProperty("ResourceManager");
+            if (prop != null && prop.PropertyType == typeof(ResourceManager) && prop.CanRead && prop.GetMethod.IsStatic)
+            {
+                // use existing resource manager if possible
+                return (ResourceManager) prop.GetValue(null);
+            }
+            
             return new ResourceManager(type);
         }
         
         internal string Format(params object[] arguments)
         {
-            var format = _resourceManager.Value?.GetString(DisplayName, CultureInfo.CurrentUICulture);
-            if (string.IsNullOrEmpty(format))
+            var format = DisplayName;
+
+            if (ResourceType != null)
             {
-                // failed to localize display name string, use it as is
-                format = DisplayName;
+                format = _resourceManagerCache
+                    .GetOrAdd(ResourceType, InitResourceManager)
+                    .GetString(DisplayName, CultureInfo.CurrentUICulture);
+
+                if (string.IsNullOrEmpty(format))
+                {
+                    // failed to localize display name string, use it as is
+                    format = DisplayName;
+                }
             }
             
             return string.Format(CultureInfo.CurrentCulture, format, arguments);
