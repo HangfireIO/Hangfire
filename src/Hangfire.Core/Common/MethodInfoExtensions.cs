24a5e6749cd021ac0ede3193b9285c4936f8f0b6
Marcus Widblom <marcus.widblom@tretton37.com>
2017-11-10 15:07:11 +0100

    Added an extension method that returns a normalized method name, fixes #1050

---
+++ src/Hangfire.Core/Common/MethodInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Hangfire.Common
+{
+    internal static class MethodInfoExtensions
+    {
+        public static string GetNormalizedName(this MethodInfo methodInfo)
+        {
+            // Method names containing '.' are considered explicitly implemented interface methods
+            // https://stackoverflow.com/a/17854048/1398672
+            return methodInfo.Name.Contains(".") && methodInfo.IsFinal && methodInfo.IsPrivate
+                ? methodInfo.Name.Split('.').Last()
+                : methodInfo.Name;
+        }
+    }
+}
