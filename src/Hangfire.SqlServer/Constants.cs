1aadb7ebe38b81b7159dbdd92b6688b92a024515
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2018-01-24 21:42:22 +0100

    #1097 | Introducing constants for default table names.

--- src/Hangfire.SqlServer/Constants.cs
+++ src/Hangfire.SqlServer/Constants.cs
@@ -3,5 +3,19 @@
     internal class Constants
     {
         public static readonly string DefaultSchema = "HangFire";
+        
+        //Default table names
+        public static readonly string DefaultNameSchemaTable = "Schema";
+        public static readonly string DefaultNameJobTable = "Job";
+        public static readonly string DefaultNameStateTable = "State";
+        public static readonly string DefaultNameJobParameterTable = "JobParameter";
+        public static readonly string DefaultNameJobQueueTable = "JobQueue";
+        public static readonly string DefaultNameServerTable = "Server";
+        public static readonly string DefaultNameHashTable = "Hash";
+        public static readonly string DefaultNameListTable = "List";
+        public static readonly string DefaultNameSetTable = "Set";
+        public static readonly string DefaultNameValueTable = "Value";
+        public static readonly string DefaultNameCounterTable = "Counter";
+        public static readonly string DefaultNameAggregatedCounterTable = "AggregatedCounter";
     }
 }
