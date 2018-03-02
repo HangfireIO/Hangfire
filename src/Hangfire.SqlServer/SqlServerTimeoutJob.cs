bcdcea9f1749e6e192d01d2356bf48ae25ac05e6
Sergey Odinokov <odinserj@gmail.com>
2018-03-02 14:30:43 +0300

    Optimize the SQL Server-based job queue implementation

--- src/Hangfire.SqlServer/SqlServerTimeoutJob.cs
+++ src/Hangfire.SqlServer/SqlServerTimeoutJob.cs
@@ -52,7 +52,7 @@ namespace Hangfire.SqlServer
                 _storage.UseConnection(null, connection =>
                 {
                     connection.Execute(
-                        $"delete from {_storage.SchemaName}.JobQueue where Queue = @queue and Id = @id",
+                        $"delete JQ from {_storage.SchemaName}.JobQueue JQ with (paglock, xlock) where Queue = @queue and Id = @id",
                         new { queue = Queue, id = Id },
                         commandTimeout: _storage.CommandTimeout);
                 });
