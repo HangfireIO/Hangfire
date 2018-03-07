0cfb7886042f107f5e9f723a51e98bce00b30491
Libin Lu <llu@evolenthealth.com>
2018-03-07 13:26:09 -0500

    fix method signature

--- src/Hangfire.Core/App_Packages/LibLog.1.4/LibLog.cs
+++ src/Hangfire.Core/App_Packages/LibLog.1.4/LibLog.cs
@@ -697,7 +697,7 @@ namespace Hangfire.Logging.LogProviders
         private static Func<string, object> GetGetLoggerMethodCall()
         {
             Type logManagerType = GetLogManagerType();
-            MethodInfo method = logManagerType.GetRuntimeMethod("GetLogger", new[] { typeof(string) });
+            MethodInfo method = logManagerType.GetRuntimeMethod("GetLogger", new[] { typeof(Assembly), typeof(string) });
             ParameterExpression nameParam = Expression.Parameter(typeof(string), "name");
             MethodCallExpression methodCall = Expression.Call(null, method, new Expression[] { Expression.Constant(typeof(Log4NetLogProvider).GetTypeInfo().Assembly), nameParam });
             return Expression.Lambda<Func<string, object>>(methodCall, new[] { nameParam }).Compile();
