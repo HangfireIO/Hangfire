b7483b96ac93d568cd8978540659cfa0bae061a5
Alexey Skalozub <pieceofsummer@gmail.com>
2018-03-12 16:29:09 +0300

    Fix issue with configuration block not called on .NET Core + reuse services (#800)
    * - Fix issue with .NET Core IoC, which may cause configuration block not being called;
    - Reuse registered services in BackgroundJobServer
    
    * - Reuse registered services in Dashboard;
    - This will also make custom IJobFilterProvider from IoC work in Dashboard
    
    * Rollback local defines which were not supposed to be committed

--- src/Hangfire.Core/BackgroundJobServer.cs
+++ src/Hangfire.Core/BackgroundJobServer.cs
@@ -23,6 +23,7 @@ using Hangfire.Common;
 using Hangfire.Logging;
 using Hangfire.Server;
 using Hangfire.States;
+using System.ComponentModel;
 
 namespace Hangfire
 {
@@ -77,15 +78,34 @@ namespace Hangfire
             [NotNull] BackgroundJobServerOptions options,
             [NotNull] JobStorage storage,
             [NotNull] IEnumerable<IBackgroundProcess> additionalProcesses)
+            : this(options, storage, additionalProcesses, 
+                   options.FilterProvider ?? JobFilterProviders.Providers,
+                   options.Activator ?? JobActivator.Current, 
+                   null, null, null)
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public BackgroundJobServer(
+            [NotNull] BackgroundJobServerOptions options,
+            [NotNull] JobStorage storage,
+            [NotNull] IEnumerable<IBackgroundProcess> additionalProcesses,
+            [NotNull] IJobFilterProvider filterProvider,
+            [NotNull] JobActivator activator,
+            [CanBeNull] IBackgroundJobFactory factory,
+            [CanBeNull] IBackgroundJobPerformer performer,
+            [CanBeNull] IBackgroundJobStateChanger stateChanger)
         {
             if (storage == null) throw new ArgumentNullException(nameof(storage));
             if (options == null) throw new ArgumentNullException(nameof(options));
             if (additionalProcesses == null) throw new ArgumentNullException(nameof(additionalProcesses));
+            if (filterProvider == null) throw new ArgumentNullException(nameof(filterProvider));
+            if (activator == null) throw new ArgumentNullException(nameof(activator));
 
             _options = options;
 
             var processes = new List<IBackgroundProcess>();
-            processes.AddRange(GetRequiredProcesses());
+            processes.AddRange(GetRequiredProcesses(filterProvider, activator, factory, performer, stateChanger));
             processes.AddRange(additionalProcesses);
 
             var properties = new Dictionary<string, object>
@@ -124,15 +144,18 @@ namespace Hangfire
             Logger.Info("Hangfire Server stopped.");
         }
 
-        private IEnumerable<IBackgroundProcess> GetRequiredProcesses()
+        private IEnumerable<IBackgroundProcess> GetRequiredProcesses(
+            [NotNull] IJobFilterProvider filterProvider,
+            [NotNull] JobActivator activator,
+            [CanBeNull] IBackgroundJobFactory factory,
+            [CanBeNull] IBackgroundJobPerformer performer,
+            [CanBeNull] IBackgroundJobStateChanger stateChanger)
         {
             var processes = new List<IBackgroundProcess>();
-
-            var filterProvider = _options.FilterProvider ?? JobFilterProviders.Providers;
-
-            var factory = new BackgroundJobFactory(filterProvider);
-            var performer = new BackgroundJobPerformer(filterProvider, _options.Activator ?? JobActivator.Current);
-            var stateChanger = new BackgroundJobStateChanger(filterProvider);
+            
+            factory = factory ?? new BackgroundJobFactory(filterProvider);
+            performer = performer ?? new BackgroundJobPerformer(filterProvider, activator);
+            stateChanger = stateChanger ?? new BackgroundJobStateChanger(filterProvider);
             
             for (var i = 0; i < _options.WorkerCount; i++)
             {
