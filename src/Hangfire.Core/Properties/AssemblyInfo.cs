b664e7a4af225128c01edcba986814a76ebd3ca5
Qui Nguyen <nvq@globeteam.com>
2018-05-04 12:43:49 +0700

    Use strong name

--- src/Hangfire.Core/Properties/AssemblyInfo.cs
+++ src/Hangfire.Core/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Core classes of Hangfire that are independent of any framework.")]
 [assembly: Guid("4deecd4f-19f6-426b-aa87-6cd1a03eaa48")]
 [assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("Hangfire.Core.Tests")]
+[assembly: InternalsVisibleTo("Hangfire.Core.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c17fd4ab888310ae53a1b5aa54f24571546c5f3dfee9d67782a28f2bc2528bb99766d2f498ad6dc45946af1f434ce3bb1b6af7b74f97f3c626345b411a3ac86f6850aef8e24af2316a4c6c823ba71ca0ca2a3643067a98ea53154a89437d451089f5b7d97e45214d821357d9e4aa6c21dc639d223caa7a4d3175f9f77806f1a5")]
 
 // Allow the generation of mocks for internal types
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c17fd4ab888310ae53a1b5aa54f24571546c5f3dfee9d67782a28f2bc2528bb99766d2f498ad6dc45946af1f434ce3bb1b6af7b74f97f3c626345b411a3ac86f6850aef8e24af2316a4c6c823ba71ca0ca2a3643067a98ea53154a89437d451089f5b7d97e45214d821357d9e4aa6c21dc639d223caa7a4d3175f9f77806f1a5")]
