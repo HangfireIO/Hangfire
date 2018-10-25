dc32f65e4c6ced4c4961b740267bef8309124330
Chuck Pennington <chuck.pennington@intel.com>
2018-10-25 15:02:30 -0700

    Fix Strings.Designer.cs compile error

--- src/Hangfire.Core/Dashboard/Content/resx/Strings.Designer.cs
+++ src/Hangfire.Core/Dashboard/Content/resx/Strings.Designer.cs
@@ -8,6 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Reflection;
+
 namespace Hangfire.Dashboard.Resources {
     using System;
     
@@ -39,7 +41,7 @@ namespace Hangfire.Dashboard.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Hangfire.Dashboard.Content.resx.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Hangfire.Dashboard.Content.resx.Strings", typeof(Strings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
