bb288a77bc207c60c72d3de0e8f869027cfc4376
Sergey Odinokov <odinserj@gmail.com>
2018-12-06 12:21:06 +0300

    Fix flapping unit test

--- tests/Hangfire.Core.Tests/States/AwaitingStateFacts.cs
+++ tests/Hangfire.Core.Tests/States/AwaitingStateFacts.cs
@@ -29,7 +29,7 @@ namespace Hangfire.Core.Tests.States
             Assert.Equal(state.ParentId, data["ParentId"]);
             Assert.Matches(
                 "^{\"\\$type\":\"Hangfire.States.EnqueuedState, Hangfire.Core\"," +
-                "\"EnqueuedAt\":\"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6,7}(Z|[+-]\\d{2}:\\d{2})\"}$", 
+                "\"EnqueuedAt\":\"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{1,7}(Z|[+-]\\d{2}:\\d{2})\"}$", 
                 data["NextState"]);
             Assert.Equal(state.Options.ToString("D"), data["Options"]);
             Assert.Equal(state.Expiration.ToString(), data["Expiration"]);
