57375781ffda55f08f4cb2a9918bf663ad1310b4
willchis <willchislett@gmail.com>
2019-01-28 16:37:52 -0800

    Checking build result of the psake-project.ps1 script for errors and returning exit code 1 for appveyor to detect.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 .nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages -Verbosity normal
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\psake-project.ps1 %*; if ($LastExitCode -and $LastExitCode -ne 0) {write-host "ERROR CODE: $LastExitCode" -fore RED; exit $lastexitcode} }"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\psake-project.ps1 %*; if ($psake.build_success -eq $false) { write-host "ERROR, build/test failure" -fore RED;  exit 1 } else { if ($LastExitCode -and $LastExitCode -ne 0) { write-host "ERROR CODE: $LastExitCode" -fore RED; exit $lastexitcode } } }"
