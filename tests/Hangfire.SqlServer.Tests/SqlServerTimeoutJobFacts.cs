715817050c726c9be3f7882aba0fd1f5df6ad02f
Sergey Odinokov <odinserj@gmail.com>
2019-04-12 13:07:25 +0300

    Update SqlServerTimeoutJobFacts.cs

--- tests/Hangfire.SqlServer.Tests/SqlServerTimeoutJobFacts.cs
+++ tests/Hangfire.SqlServer.Tests/SqlServerTimeoutJobFacts.cs
@@ -137,7 +137,7 @@ namespace Hangfire.SqlServer.Tests
 
                     var record = sql.Query("select * from HangFire.JobQueue").Single();
 
-                    Assert.Equal(processingJob.FetchedAt, record.FetchedAt);
+                    Assert.Equal<DateTime?>(processingJob.FetchedAt, record.FetchedAt);
                     var now = DateTime.UtcNow;
                     Assert.True(now.AddSeconds(-5) < record.FetchedAt, (now - record.FetchedAt).ToString());
                 }
