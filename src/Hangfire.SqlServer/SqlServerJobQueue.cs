a21b12f12e27db82ccf885ad810064bcba6a4767
isafar <isafar@DEV03WIN12>
2019-09-12 08:45:29 +0200

    NonBlockingFetchSql override _options.QueuePollInterval < LongPollingThreshold

--- src/Hangfire.SqlServer/SqlServerJobQueue.cs
+++ src/Hangfire.SqlServer/SqlServerJobQueue.cs
@@ -144,7 +144,7 @@ $@"insert into [{_storage.SchemaName}].JobQueue (JobId, Queue) values (@jobId, @
                         throw new InvalidOperationException($"A call to sp_getapplock returned unexpected result '{lockResult.Value}' while fetching a job. Please report this problem to Hangfire developers and don't use sub-second values for the QueuePollInterval option.");
                     }
 
-                    if (_options.QueuePollInterval < LongPollingThreshold)
+                    if (_options.QueuePollInterval < LongPollingThreshold  && !_options.NonBlockingFetchSql)
                     {
                         isBlocking = true;
                     }
