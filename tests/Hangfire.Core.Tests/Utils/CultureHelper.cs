88345083952b3a8fccc80e5c03099f37e227674f
Sergey Odinokov <odinserj@gmail.com>
2019-12-04 15:50:45 +0300

    Allow to capture and restore InvariantCulture for background jobs
    Closes #1562

--- tests/Hangfire.Core.Tests/Utils/CultureHelper.cs
+++ tests/Hangfire.Core.Tests/Utils/CultureHelper.cs
@@ -5,6 +5,24 @@ namespace Hangfire.Core.Tests
 {
     internal static class CultureHelper
     {
+        public static void SetCurrentCulture(CultureInfo cultureInfo)
+        {
+#if !NETCOREAPP1_0
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+#else
+            CultureInfo.CurrentCulture = cultureInfo;
+#endif
+        }
+
+        public static void SetCurrentUICulture(CultureInfo cultureInfo)
+        {
+#if !NETCOREAPP1_0
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+#else
+            CultureInfo.CurrentUICulture = cultureInfo;
+#endif
+        }
+
         public static void SetCurrentCulture(string id)
         {
 #if !NETCOREAPP1_0
