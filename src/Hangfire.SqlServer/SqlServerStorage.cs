467195bc35110214c7698f0275d3d0714a296d9d
Sergey Odinokov <odinserj@gmail.com>
2020-01-14 12:49:27 +0300

    Don't leak DbConnection when an exception occurs during its opening

--- src/Hangfire.SqlServer/SqlServerStorage.cs
+++ src/Hangfire.SqlServer/SqlServerStorage.cs
@@ -1,5 +1,5 @@
 // This file is part of Hangfire.
-// Copyright © 2013-2014 Sergey Odinokov.
+// Copyright Â© 2013-2014 Sergey Odinokov.
 // 
 // Hangfire is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as 
@@ -267,15 +267,24 @@ namespace Hangfire.SqlServer
 
         internal DbConnection CreateAndOpenConnection()
         {
-            var connection = _existingConnection
-                ?? _connectionFactory();
+            DbConnection connection = null;
 
-            if (connection.State == ConnectionState.Closed)
+            try
             {
-                connection.Open();
-            }
+                connection = _existingConnection ?? _connectionFactory();
 
-            return connection;
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                return connection;
+            }
+            catch
+            {
+                ReleaseConnection(connection);
+                throw;
+            }
         }
 
         internal bool IsExistingConnection(IDbConnection connection)
