18e066cbef815abc3daa02d0454db96958d3fc7b
Ben Elliott <4996462+benelliott@users.noreply.github.com>
2020-01-21 17:54:34 +0000

    Add abstract JobMetadataAttribute to share ResourceManager logic between JobDescriptionAttribute and JobDisplayNameAttribute

--- src/Hangfire.Core/JobDescriptionAttribute.cs
+++ src/Hangfire.Core/JobDescriptionAttribute.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using System.Resources;
-using Hangfire.Common;
-using Hangfire.Dashboard;
 
 namespace Hangfire
 {
@@ -13,59 +6,15 @@ namespace Hangfire
     /// Specifies an HTML description for a job method.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class JobDescriptionAttribute : Attribute
+    public class JobDescriptionAttribute : JobMetadataAttribute
     {
-        private static readonly ConcurrentDictionary<Type, ResourceManager> _resourceManagerCache
-            = new ConcurrentDictionary<Type, ResourceManager>();
-
-        public JobDescriptionAttribute(string description)
+        public JobDescriptionAttribute(string metadataValue) : base(metadataValue)
         {
-            if (string.IsNullOrEmpty(description))
-                throw new ArgumentException("Description is empty", nameof(description));
-
-            Description = description;
         }
 
         /// <summary>
         /// Gets description for the job.
         /// </summary>
-        public string Description { get; }
-
-        /// <summary>
-        /// Gets or sets resource type to localize <see cref="Description"/> string.
-        /// </summary>
-        public Type ResourceType { get; set; }
-
-        public virtual string Format(DashboardContext context, Job job)
-        {
-            var format = Description;
-
-            if (ResourceType != null)
-            {
-                format = _resourceManagerCache
-                    .GetOrAdd(ResourceType, InitResourceManager)
-                    .GetString(Description, CultureInfo.CurrentUICulture);
-
-                if (string.IsNullOrEmpty(format))
-                {
-                    // failed to localize description string, use it as is
-                    format = Description;
-                }
-            }
-            
-            return string.Format(CultureInfo.CurrentCulture, format, job.Args.ToArray());
-        }
-
-        private static ResourceManager InitResourceManager(Type type)
-        {
-            var prop = type.GetTypeInfo().GetDeclaredProperty("ResourceManager");
-            if (prop != null && prop.PropertyType == typeof(ResourceManager) && prop.CanRead && prop.GetMethod.IsStatic)
-            {
-                // use existing resource manager if possible
-                return (ResourceManager)prop.GetValue(null);
-            }
-
-            return new ResourceManager(type);
-        }
+        public string Description => MetadataValue;
     }
 }
---
+++ src/Hangfire.Core/JobMetadataAttribute.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using Hangfire.Common;
+using Hangfire.Dashboard;
+
+namespace Hangfire
+{
+    /// <summary>
+    /// A base class that represents attributes that provide additional string metadata about a job, such as a name
+    /// or a description.
+    /// </summary>
+    public abstract class JobMetadataAttribute : Attribute
+    {
+        private static readonly ConcurrentDictionary<Type, ResourceManager> _resourceManagerCache
+            = new ConcurrentDictionary<Type, ResourceManager>();
+
+        public JobMetadataAttribute(string metadataValue)
+        {
+            if (string.IsNullOrEmpty(metadataValue))
+                throw new ArgumentException("Metadata value is empty", nameof(metadataValue));
+
+            MetadataValue = metadataValue;
+        }
+
+        /// <summary>
+        /// Gets the metadata for this attribute.
+        /// </summary>
+        public string MetadataValue { get; }
+
+        /// <summary>
+        /// Gets or sets resource type to localize the metadata string.
+        /// </summary>
+        public Type ResourceType { get; set; }
+
+        public string Format(DashboardContext context, Job job)
+        {
+            var formatted = MetadataValue;
+
+            if (ResourceType != null)
+            {
+                formatted = _resourceManagerCache
+                    .GetOrAdd(ResourceType, InitResourceManager)
+                    .GetString(MetadataValue, CultureInfo.CurrentUICulture);
+
+                if (string.IsNullOrEmpty(formatted))
+                {
+                    // failed to localize description string, use it as is
+                    formatted = MetadataValue;
+                }
+            }
+            
+            return string.Format(CultureInfo.CurrentCulture, formatted, job.Args.ToArray());
+        }
+
+        private static ResourceManager InitResourceManager(Type type)
+        {
+            var prop = type.GetTypeInfo().GetDeclaredProperty("ResourceManager");
+            if (prop != null && prop.PropertyType == typeof(ResourceManager) && prop.CanRead && prop.GetMethod.IsStatic)
+            {
+                // use existing resource manager if possible
+                return (ResourceManager)prop.GetValue(null);
+            }
+
+            return new ResourceManager(type);
+        }
+    }
+}
