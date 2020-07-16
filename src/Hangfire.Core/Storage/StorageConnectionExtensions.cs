f4a3a5f10242a48c8567a5f3df7331dcc06d03e9
Sergey Odinokov <odinserj@gmail.com>
2020-07-16 16:24:55 +0300

    Expose IStorageConnection.GetRecurringJobs(ids) to public

--- src/Hangfire.Core/Storage/StorageConnectionExtensions.cs
+++ src/Hangfire.Core/Storage/StorageConnectionExtensions.cs
@@ -61,6 +61,12 @@ namespace Hangfire.Storage
             return GetRecurringJobDtos(connection, ids);
         }
 
+        public static List<RecurringJobDto> GetRecurringJobs([NotNull] this IStorageConnection connection, IEnumerable<string> ids)
+        {
+            if (connection == null) throw new ArgumentNullException(nameof(connection));
+            return GetRecurringJobDtos(connection, ids);
+        }
+
         private static List<RecurringJobDto> GetRecurringJobDtos(IStorageConnection connection, IEnumerable<string> ids)
         {
             var result = new List<RecurringJobDto>();
@@ -68,6 +74,7 @@ namespace Hangfire.Storage
             {
                 var hash = connection.GetAllEntriesFromHash($"recurring-job:{id}");
 
+                // TODO: Remove this in 2.0 (breaking change)
                 if (hash == null)
                 {
                     result.Add(new RecurringJobDto { Id = id, Removed = true });
