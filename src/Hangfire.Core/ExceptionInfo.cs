c618b853102cb25347712218631a708edb7f7cbd
Sergey Odinokov <odinserj@gmail.com>
2021-03-16 17:05:23 +0300

    Annotate the ExceptionInfo class

--- src/Hangfire.Core/ExceptionInfo.cs
+++ src/Hangfire.Core/ExceptionInfo.cs
@@ -15,16 +15,16 @@
 // License along with Hangfire. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.ComponentModel;
 using System.Text;
+using Hangfire.Annotations;
 using Hangfire.Common;
 using Newtonsoft.Json;
 
 namespace Hangfire
 {
-    public class ExceptionInfo
+    public sealed class ExceptionInfo
     {
-        public ExceptionInfo(Exception exception)
+        public ExceptionInfo([NotNull] Exception exception)
         {
             if (exception == null) throw new ArgumentNullException(nameof(exception));
 
@@ -38,19 +38,22 @@ namespace Hangfire
         }
 
         [JsonConstructor]
-        public ExceptionInfo(string type, string message, ExceptionInfo innerException)
+        public ExceptionInfo([NotNull] string type, [CanBeNull] string message, [CanBeNull] ExceptionInfo innerException)
         {
-            Type = type;
+            Type = type ?? throw new ArgumentNullException(nameof(type));
             Message = message;
             InnerException = innerException;
         }
 
+        [NotNull]
         [JsonProperty("e")]
         public string Type { get; }
 
+        [CanBeNull]
         [JsonProperty("m", NullValueHandling = NullValueHandling.Ignore)]
         public string Message { get; }
 
+        [CanBeNull]
         [JsonProperty("i", NullValueHandling = NullValueHandling.Ignore)]
         public ExceptionInfo InnerException { get; }
 
