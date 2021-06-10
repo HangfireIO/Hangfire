92c938ad64e646d9846df021f5f881644b07d491
Sergey Odinokov <odinserj@gmail.com>
2021-06-10 15:14:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ then
   sudo docker run --name=mssql2017 -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Password12!' -p 1433:1433 -d microsoft/mssql-server-linux:2017-latest;
   export Hangfire_SqlServer_ConnectionStringTemplate="Server=tcp:127.0.0.1,1433;Database={0};User Id=sa;Password=Password12!";
 fi
+
 if hash mono 2>/dev/null; 
 then
   nuget install .nuget/packages.config -OutputDirectory packages;
@@ -16,6 +17,7 @@ then
     mono ./packages/xunit.runner.console.2.2.0/tools/xunit.console.exe ./tests/Hangfire.SqlServer.Tests/bin/Release/net452/Hangfire.SqlServer.Tests.dll;
   fi
 fi
+
 if hash dotnet 2>/dev/null; 
 then
   dotnet test -c Release -f netcoreapp3.1 tests/Hangfire.Core.Tests/Hangfire.Core.Tests.csproj;
