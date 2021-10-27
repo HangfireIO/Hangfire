523d491e3ad21c7208070730107dac33cda78e4d
Sergey Odinokov <odinserj@gmail.com>
2021-10-27 18:15:08 +0300

    Add unit test to check default authorization filters

--- tests/Hangfire.Core.Tests/Dashboard/DashboardOptionsFacts.cs
+++ tests/Hangfire.Core.Tests/Dashboard/DashboardOptionsFacts.cs
@@ -1,4 +1,5 @@
-﻿using Hangfire.Dashboard;
+﻿using System.Linq;
+using Hangfire.Dashboard;
 using Xunit;
 
 namespace Hangfire.Core.Tests.Dashboard
@@ -12,6 +13,7 @@ namespace Hangfire.Core.Tests.Dashboard
             Assert.Equal(options.AppPath, "/");
             Assert.Equal(options.PrefixPath, "");
             Assert.NotNull(options.Authorization);
+            Assert.IsType<LocalRequestsOnlyAuthorizationFilter>(options.Authorization.FirstOrDefault());
             Assert.Equal(options.StatsPollingInterval, 2000);
             Assert.Equal(options.DisplayStorageConnectionString,true);
             Assert.Equal(options.DashboardTitle, "Hangfire Dashboard");
