bb7a1cd7c7d6aaeb0ff7d56566e9049e4b7fc206
Sergey Odinokov <odinserj@hangfire.io>
2023-08-24 11:38:42 +0700

    Use the invariant culture when formatting key names for metrics
    Possibly relates to #2245

--- src/Hangfire.Core/StatisticsHistoryAttribute.cs
+++ src/Hangfire.Core/StatisticsHistoryAttribute.cs
@@ -36,27 +36,27 @@ namespace Hangfire
                     DateTime.UtcNow.AddMonths(1) - DateTime.UtcNow);
 
                 context.Transaction.IncrementCounter(
-                    $"stats:succeeded:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH")}",
+                    $"stats:succeeded:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH", CultureInfo.InvariantCulture)}",
                     TimeSpan.FromDays(1));
             }
             else if (context.CandidateState.Name == FailedState.StateName)
             {
                 context.Transaction.IncrementCounter(
-                    $"stats:failed:{DateTime.UtcNow.ToString("yyyy-MM-dd")}",
+                    $"stats:failed:{DateTime.UtcNow.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}",
                     DateTime.UtcNow.AddMonths(1) - DateTime.UtcNow);
 
                 context.Transaction.IncrementCounter(
-                    $"stats:failed:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH")}",
+                    $"stats:failed:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH", CultureInfo.InvariantCulture)}",
                     TimeSpan.FromDays(1));
             }
             else if (context.CandidateState is DeletedState)
             {
                 context.Transaction.IncrementCounter(
-                    $"stats:deleted:{DateTime.UtcNow.ToString("yyyy-MM-dd")}",
+                    $"stats:deleted:{DateTime.UtcNow.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}",
                     DateTime.UtcNow.AddMonths(1) - DateTime.UtcNow);
 
                 context.Transaction.IncrementCounter(
-                    $"stats:deleted:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH")}",
+                    $"stats:deleted:{DateTime.UtcNow.ToString("yyyy-MM-dd-HH", CultureInfo.InvariantCulture)}",
                     TimeSpan.FromDays(1));
             }
         }
