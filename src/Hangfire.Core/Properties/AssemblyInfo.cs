4ad9344c6b85e4105c63c342a7772faf0b5952c7
Sergey Odinokov <odinserj@hangfire.io>
2023-08-24 14:26:54 +0700

    Specify the `NeutralResourcesLanguage("en")` for Hangfire.Core assembly

--- src/Hangfire.Core/Properties/AssemblyInfo.cs
+++ src/Hangfire.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("4deecd4f-19f6-426b-aa87-6cd1a03eaa48")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("Hangfire.Core.Tests")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Allow the generation of mocks for internal types
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
