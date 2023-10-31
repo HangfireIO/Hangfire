4ddfac7e69257c5f96b3dbed1f072ef6d6264765
Sergey Odinokov <odinserj@hangfire.io>
2023-10-31 16:24:57 +0700

    Bump version to 1.8.7

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Please don't edit it manually, use the `build.bat version` command instead.
-[assembly: AssemblyVersion("1.8.6")]
+[assembly: AssemblyVersion("1.8.7")]
