fd7690638482ccf02c90212434b8c5cdf8e65234
Sergey Odinokov <odinserj@hangfire.io>
2024-10-17 16:21:00 +0700

    Move out the `DbCommandExtensions` class to its own file

--- src/Hangfire.SqlServer/SqlCommandBatch.cs
+++ src/Hangfire.SqlServer/SqlCommandBatch.cs
@@ -15,77 +15,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Common;
-using Hangfire.Annotations;
 
 namespace Hangfire.SqlServer
 {
-    internal static class DbCommandExtensions
-    {
-        public static DbCommand Create(
-            [NotNull] this DbConnection connection,
-            [NotNull] string text,
-            CommandType type = CommandType.Text,
-            int? timeout = null)
-        {
-            if (connection == null) throw new ArgumentNullException(nameof(connection));
-            if (text == null) throw new ArgumentNullException(nameof(text));
-
-            var command = connection.CreateCommand();
-            command.CommandType = type;
-            command.CommandText = text;
-
-            if (timeout.HasValue)
-            {
-                command.CommandTimeout = timeout.Value;
-            }
-
-            return command;
-        }
-
-        public static DbCommand AddParameter(
-            [NotNull] this DbCommand command,
-            [NotNull] string parameterName,
-            [CanBeNull] object value,
-            DbType dbType,
-            [CanBeNull] int? size = null)
-        {
-            if (command == null) throw new ArgumentNullException(nameof(command));
-            if (parameterName == null) throw new ArgumentNullException(nameof(parameterName));
-
-            var parameter = command.CreateParameter();
-            parameter.ParameterName = parameterName;
-            parameter.DbType = dbType;
-            parameter.Value = value ?? DBNull.Value;
-
-            if (size.HasValue) parameter.Size = size.Value;
-
-            command.Parameters.Add(parameter);
-            return command;
-        }
-
-        public static DbCommand AddReturnParameter(
-            [NotNull] this DbCommand command,
-            string parameterName,
-            out DbParameter parameter,
-            DbType dbType,
-            int? size = null)
-        {
-            if (command == null) throw new ArgumentNullException(nameof(command));
-
-            parameter = command.CreateParameter();
-            parameter.ParameterName = parameterName;
-            parameter.DbType = dbType;
-            parameter.Direction = ParameterDirection.ReturnValue;
-
-            if (size.HasValue) parameter.Size = size.Value;
-
-            command.Parameters.Add(parameter);
-            return command;
-        }
-    }
-
     internal sealed class SqlCommandBatch : IDisposable
     {
         private readonly List<DbCommand> _commandList = new List<DbCommand>();
